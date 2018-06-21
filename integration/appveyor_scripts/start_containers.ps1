ef2f3ed17b89bf73b8d4838885a8138a69ecc8b7
Matthew Hodgkins <matthodge@gmail.com>
2018-06-21 22:28:42 +0200

    Refactor Docker Compose Start

--- integration/appveyor_scripts/start_containers.ps1
+++ integration/appveyor_scripts/start_containers.ps1
@@ -1,16 +1,86 @@
-Push-Location -Path 'integration\appveyor_scripts'
-Write-Output "Starting docker-compose. This may take a while..."
-Start-Process -FilePath 'docker-compose' -ArgumentList 'up -d' -Wait -NoNewWindow
-Pop-Location
+function Start-DockerCompose
+{
+    [CmdletBinding()]
+    Param
+    (
+        # Directory to start Docker Compose In
+        [Parameter(Mandatory=$false)]
+        [string]
+        $WorkingDirectory=$PWD.Path,
 
-# Wait for Octopus to be ready
+        # Arguments to start Docker Compose With
+        [Parameter(Mandatory=$true)]
+        [string]
+        $ArgumentList
+    )
+
+    $ErrorActionPreference = 'Stop'
+
+    # Setup stdin\stdout redirection
+    $StartInfo = New-Object System.Diagnostics.ProcessStartInfo -Property @{
+                    FileName = 'docker-compose'
+                    Arguments = 'up -d'
+                    UseShellExecute = $false
+                    RedirectStandardOutput = $true
+                    RedirectStandardError = $true
+                    WorkingDirectory = $WorkingDirectory
+                }
+
+    # Create new process
+    $Process = New-Object System.Diagnostics.Process
+
+    # Assign previously created StartInfo properties
+    $Process.StartInfo = $StartInfo
+
+    # Register Object Events for stdin\stdout reading
+    $OutEvent = Register-ObjectEvent -InputObject $Process -EventName OutputDataReceived -Action {
+        Write-Host $Event.SourceEventArgs.Data
+    }
+    $ErrEvent = Register-ObjectEvent -InputObject $Process -EventName ErrorDataReceived -Action {
+        Write-Host $Event.SourceEventArgs.Data
+    }
+
+    # Start process
+    [void]$Process.Start()
+
+    # Begin reading stdin\stdout
+    $Process.BeginOutputReadLine()
+    $Process.BeginErrorReadLine()
+
+    # Start a timer to provide some feedback
+    $stopwatch =  [system.diagnostics.stopwatch]::StartNew()
+
+    # Do something else while events are firing
+    do
+    {
+
+        Start-Sleep -Seconds 60
+        Write-Output "Currently waited $($stopwatch.Elapsed.Minutes) minutes for docker-compose to finish.."
+    }
+    while (!$Process.HasExited)
+
+    # Unregister events
+    $OutEvent.Name, $ErrEvent.Name |
+        ForEach-Object {Unregister-Event -SourceIdentifier $_}
+}
+
+Write-Output "Starting docker-compose. This may take several minutes"
+
+$workingDirectory = Join-Path -Path $PWD -ChildPath 'integration\appveyor_scripts'
+
+Start-DockerCompose -WorkingDirectory $workingDirectory -ArgumentList "up -d"
+
 $octopusReady = $false
-$retryCount
+$maxRetries = 10
 
-$localMachineIP = (Test-Connection -ComputerName (hostname) -Count 1  | Select IPV4Address).IPV4Address
+$localMachineIP = Get-NetAdapter | Where-Object { $_.Name -like "*DockerNAT*" } | Get-NetIPAddress | Where-Object { $_.AddressFamily -eq 'IPv4' }
+$localMachineIP = $localMachineIP.IPAddress
+
+if ([string]::IsNullOrEmpty($localMachineIP)) {
+    Write-Error "Cannot get Docker Adapaters IP Address"
+}
 
 Write-Output "Machine IP Address: $($localMachineIP)"
-
 
 do {
     try {
@@ -27,9 +97,9 @@ do {
 
     $retryCount++
 
-}until($retryCount -eq 10 -or $octopusReady)
+}until($retryCount -eq $maxRetries -or $octopusReady)
 
-if ($retryCount -eq 10)
+if ($retryCount -eq $maxRetries)
 {
     Write-Error "Failed to bring up Octopus Deploy container correctly."
 }
