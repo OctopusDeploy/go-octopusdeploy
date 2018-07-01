c696418e19ffde77c96bea2a5595557d673c0c07
Matthew Hodgkins <matthodge@gmail.com>
2018-07-01 22:17:53 +0200

    Refactor + Add Tests for Deployment Processes

--- octopusdeploy/deployment_process.go
+++ octopusdeploy/deployment_process.go
@@ -18,66 +18,91 @@ func NewDeploymentProcessService(sling *sling.Sling) *DeploymentProcessService {
 }
 
 type DeploymentProcesses struct {
-	Items []DeploymentProcess `json:"Items"`
+	Items []DeploymentProcessResource `json:"Items"`
 	PagedResults
 }
 
-type DeploymentProcess struct {
-	ID             string                   `json:"Id"`
-	ProjectID      string                   `json:"ProjectId"`
-	Steps          []DeploymentStepResource `json:"Steps"`
-	Version        int                      `json:"Version"`
-	LastSnapshotID string                   `json:"LastSnapshotId"`
-	LastModifiedOn string                   `json:"LastModifiedOn"` // date time
-	LastModifiedBy string                   `json:"LastModifiedBy"`
-	Links          Links                    `json:"Links"`
+type DeploymentProcessResource struct {
+
+	// Id
+	ID string `json:"Id,omitempty"`
+
+	// last modified by
+	LastModifiedBy string `json:"LastModifiedBy,omitempty"`
+
+	// last modified on
+	// Format: date-time
+	LastModifiedOn string `json:"LastModifiedOn,omitempty"`
+
+	// last snapshot Id
+	LastSnapshotID string `json:"LastSnapshotId,omitempty"`
+
+	// links
+	Links Links `json:"Links,omitempty"`
+
+	// project Id
+	ProjectID string `json:"ProjectId,omitempty"`
+
+	// steps
+	Steps []DeploymentStepResource `json:"Steps"`
+
+	// version
+	// Required: true
+	Version *int32 `json:"Version"`
 }
 
-func (d *DeploymentProcessService) Get(deploymentProcessID string) (DeploymentProcess, error) {
-	deploymentProcess := new(DeploymentProcess)
+func (d *DeploymentProcessService) Get(deploymentProcessID string) (*DeploymentProcessResource, error) {
+	var deploymentProcess DeploymentProcessResource
 	octopusDeployError := new(APIError)
 	path := fmt.Sprintf("deploymentprocesses/%s", deploymentProcessID)
 
-	resp, err := d.sling.New().Get(path).Receive(deploymentProcess, octopusDeployError)
+	resp, err := d.sling.New().Get(path).Receive(&deploymentProcess, &octopusDeployError)
 
 	if err != nil {
-		return *deploymentProcess, fmt.Errorf("cannot get deploymentprocess id %s from server. failure from http client %v", deploymentProcessID, err)
+		return nil, fmt.Errorf("cannot get deploymentprocess id %s from server. failure from http client %v", deploymentProcessID, err)
 	}
 
 	if resp.StatusCode != http.StatusOK {
-		return *deploymentProcess, fmt.Errorf("cannot get deploymentprocess id %s from server. response from server %s", deploymentProcessID, resp.Status)
+		return nil, fmt.Errorf("cannot get deploymentprocess id %s from server. response from server %s", deploymentProcessID, resp.Status)
 	}
 
-	return *deploymentProcess, err
+	return &deploymentProcess, err
 }
 
-func (d *DeploymentProcessService) GetAll() ([]DeploymentProcess, error) {
-	var listOfDeploymentProcess []DeploymentProcess
+func (d *DeploymentProcessService) GetAll() (*[]DeploymentProcessResource, error) {
+	var listOfDeploymentProcess []DeploymentProcessResource
 	path := fmt.Sprintf("deploymentprocesses")
 
 	for {
-		deploymentProcesses := new(DeploymentProcesses)
-		octopusDeployError := new(APIError)
+		var deploymentProcesses DeploymentProcesses
+		var octopusDeployError APIError
 
-		resp, err := d.sling.New().Get(path).Receive(deploymentProcesses, octopusDeployError)
+		resp, err := d.sling.New().Get(path).Receive(&deploymentProcesses, &octopusDeployError)
+
 		if err != nil {
 			return nil, err
 		}
 
-		fmt.Printf("Response: %v", resp.Status)
-		fmt.Printf("Total Results: %d", deploymentProcesses.NumberOfPages)
+		defer resp.Body.Close()
+
+		if octopusDeployError.Errors != nil {
+			return nil, fmt.Errorf("cannot get all deployment processes. response from octopusdeploy %s: ", octopusDeployError.Errors)
+		}
+
+		if resp.StatusCode != http.StatusOK {
+			return nil, fmt.Errorf("cannot get all projects. response from server %s", resp.Status)
+		}
 
 		for _, deploymentProcess := range deploymentProcesses.Items {
 			listOfDeploymentProcess = append(listOfDeploymentProcess, deploymentProcess)
 		}
 
 		if deploymentProcesses.PagedResults.Links.PageNext != "" {
-			fmt.Printf("More pages to go! Next link: %s", deploymentProcesses.PagedResults.Links.PageNext)
 			path = deploymentProcesses.PagedResults.Links.PageNext
 		} else {
 			break
 		}
 	}
 
-	return listOfDeploymentProcess, nil // no more pages to go through
+	return &listOfDeploymentProcess, nil // no more pages to go through
 }
