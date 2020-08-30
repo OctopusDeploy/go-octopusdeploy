c94122165227298446e1d3a1ebd1eb4f50d5b9d5
John Bristowe <john.bristowe@octopus.com>
2020-08-31 09:48:33 +1000

    Added documentation

--- octopusdeploy/account_service.go
+++ octopusdeploy/account_service.go
@@ -2,10 +2,13 @@ package octopusdeploy
 
 import "github.com/dghubble/sling"
 
+// AccountService handles communication with Account-related methods of the
+// Octopus API.
 type AccountService struct {
 	sling *sling.Sling
 }
 
+// NewAccountService returns an AccountService with a preconfigured client.
 func NewAccountService(sling *sling.Sling) *AccountService {
 	return &AccountService{
 		sling: sling,
