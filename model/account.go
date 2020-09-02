31b95451d4d3eef55437a4f3a731cedf61196e6f
John Bristowe <john.bristowe@octopus.com>
2020-09-03 06:54:42 +1000

    Changed capitalization

--- model/account.go
+++ model/account.go
@@ -7,15 +7,15 @@ import (
 	"github.com/OctopusDeploy/go-octopusdeploy/enum"
 )
 
-// Accounts defines a collection of Account types with built-in support for
-// paged results from the API
+// Accounts defines a collection of accounts with built-in support for paged
+// results.
 type Accounts struct {
 	Items []Account `json:"Items"`
 	PagedResults
 }
 
 // Account represents account details used for deployments, including
-// username/password, tokens, Azure and AWS credentials, and SSH key pairs
+// username/password, tokens, Azure and AWS credentials, and SSH key pairs.
 type Account struct {
 	AccountType                     enum.AccountType            `json:"AccountType"`
 	Description                     string                      `json:"Description,omitempty"`
@@ -23,7 +23,7 @@ type Account struct {
 	Name                            string                      `json:"Name"`
 	TenantedDeploymentParticipation enum.TenantedDeploymentMode `json:"TenantedDeploymentParticipation"`
 	TenantTags                      []string                    `json:"TenantTags"`
-	TenantIds                       []string                    `json:"TenantIds"`
+	TenantIDs                       []string                    `json:"TenantIds"`
 	SpaceID                         string                      `json:"SpaceId,omitempty"`
 	Token                           *SensitiveValue             `json:"Token,omitempty"`
 	Username                        string                      `json:"Username,omitempty"`
@@ -33,7 +33,7 @@ type Account struct {
 	Resource
 }
 
-// NewAccount initializes an Account with a name and account type
+// NewAccount initializes an account with a name and account type.
 func NewAccount(name string, accountType enum.AccountType) (*Account, error) {
 	if len(strings.Trim(name, " ")) == 0 {
 		return nil, errors.New("client: invalid account name")
