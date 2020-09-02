feb3f6ed0308716feac94a896c20966d756055fe
John Bristowe <john.bristowe@octopus.com>
2020-09-02 20:14:28 +1000

    Added comment for routing traffic through proxy trace

--- integration/test_helpers.go
+++ integration/test_helpers.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 	"log"
 	"net/http"
-	"net/url"
 	"os"
 	"time"
 
@@ -26,17 +25,22 @@ func initTest() *client.Client {
 		log.Fatal("Please make sure to set the env variables 'OCTOPUS_URL' and 'OCTOPUS_APIKEY' before running this test")
 	}
 
-	proxyStr := "http://127.0.0.1:5555"
-	proxyURL, err := url.Parse(proxyStr)
-	if err != nil {
-		log.Println(err)
-	}
+	// NOTE: You can direct traffic through a proxy trace like Fiddler
+	// Everywhere by preconfiguring the client to route traffic through a
+	// proxy.
+	//
+	// proxyStr := "http://127.0.0.1:5555"
+	// proxyURL, err := url.Parse(proxyStr)
+	// if err != nil {
+	// 	log.Println(err)
+	// }
 
-	tr := &http.Transport{
-		Proxy: http.ProxyURL(proxyURL),
-	}
-	httpClient := http.Client{Transport: tr}
+	// tr := &http.Transport{
+	// 	Proxy: http.ProxyURL(proxyURL),
+	// }
+	// httpClient := http.Client{Transport: tr}
 
+	httpClient := http.Client{}
 	octopusClient, err := client.NewClient(&httpClient, octopusURL, octopusAPIKey)
 	if err != nil {
 		log.Fatal(err)
