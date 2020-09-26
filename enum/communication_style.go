77c59a9e2e3c96749bc14190c5f5bc1911e776eb
John Bristowe <john.bristowe@octopus.com>
2020-09-26 15:52:22 +1000

    Added communication style

---
+++ enum/communication_style.go
@@ -0,0 +1,18 @@
+package enum
+
+// CommunicationStyle indicates the communication style used by a machine.
+/*
+ENUM(
+	None
+	TentaclePassive
+	TentacleActive
+	Ssh
+	OfflineDrop
+	AzureWebApp
+	Ftp
+	AzureCloudService
+	AzureServiceFabricCluster
+	Kubernetes
+)
+*/
+type CommunicationStyle int
