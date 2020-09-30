9ee2c9752ae9116b0a06b5cc361b06fc9ec9aa8f
John Bristowe <john.bristowe@octopus.com>
2020-09-30 16:59:59 +1000

    Added action type constants

---
+++ model/constants_action_types.go
@@ -0,0 +1,11 @@
+package model
+
+const (
+	ActionTypeAWSApplyCloudFormationChangeset string = "Octopus.AwsApplyCloudFormationChangeSet"
+	ActionTypeAWSDeleteCloudFormation         string = "Octopus.AwsDeleteCloudFormation"
+	ActionTypeAWSRunCloudFormation            string = "Octopus.AwsRunCloudFormation"
+	ActionTypeAWSRunScript                    string = "Octopus.AwsRunScript"
+	ActionTypeAWSUploadS3                     string = "Octopus.AwsUploadS3"
+	ActionTypeOctopusScript                   string = "Octopus.Script"
+	ActionTypeOctopusActionScriptBody         string = "Octopus.Action.Script.ScriptBody"
+)
