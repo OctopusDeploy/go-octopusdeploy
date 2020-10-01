4c7ce8b9cbf71bc469fbdc5e670f7d5a427dc022
John Bristowe <john.bristowe@octopus.com>
2020-10-02 08:50:40 +1000

    Updated documentation

--- model/user_authentication.go
+++ model/user_authentication.go
@@ -13,27 +13,31 @@ type UserAuthentication struct {
 	Resource
 }
 
-// GetID returns the ID value of the UserAuthentication.
+// GetID returns the ID value of the user authentication.
 func (resource UserAuthentication) GetID() string {
 	return resource.ID
 }
 
-// GetLastModifiedBy returns the name of the account that modified the value of this UserAuthentication.
+// GetLastModifiedBy returns the name of the account that modified the value of
+// this user authentication.
 func (resource UserAuthentication) GetLastModifiedBy() string {
 	return resource.LastModifiedBy
 }
 
-// GetLastModifiedOn returns the time when the value of this UserAuthentication was changed.
+// GetLastModifiedOn returns the time when the value of this user
+// authentication was changed.
 func (resource UserAuthentication) GetLastModifiedOn() *time.Time {
 	return resource.LastModifiedOn
 }
 
-// GetLinks returns the associated links with the value of this UserAuthentication.
+// GetLinks returns the associated links with the value of this user
+// authentication.
 func (resource UserAuthentication) GetLinks() map[string]string {
 	return resource.Links
 }
 
-// Validate checks the state of the UserAuthentication and returns an error if invalid.
+// Validate checks the state of the user authentication and returns an error if
+// invalid.
 func (resource UserAuthentication) Validate() error {
 	validate := validator.New()
 	err := validate.Struct(resource)
