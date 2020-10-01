aa211278a876db6d9aca98e7b6825003de2ef78e
John Bristowe <john.bristowe@octopus.com>
2020-10-02 09:45:34 +1000

    Updated to represent nil fields

--- model/deployment_action_container.go
+++ model/deployment_action_container.go
@@ -1,6 +1,6 @@
 package model
 
 type DeploymentActionContainer struct {
-	Image  string `json:"Image"`
-	FeedID string `json:"FeedId"`
+	Image  *string `json:"Image"`
+	FeedID *string `json:"FeedId"`
 }
