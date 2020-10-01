279c9aa7b663d44c55d042a32459d73ce1428050
John Bristowe <john.bristowe@octopus.com>
2020-10-02 08:58:32 +1000

    Updated to reflect type in API (not a Resource type)

--- model/user_authentication.go
+++ model/user_authentication.go
@@ -1,52 +1,9 @@
 package model
 
-import (
-	"time"
-
-	"github.com/go-playground/validator/v10"
-)
-
+// UserAuthentication represents enabled authentication providers and whether
+// the current user can edit logins for the given user.
 type UserAuthentication struct {
-	AuthenticationProviders             []*AuthenticationProviderElement `json:"AuthenticationProviders,omitempty"`
-	CanCurrentUserEditIdentitiesForUser *bool                            `json:"AutoLoginEnabled,omitempty"`
-
-	Resource
+	AuthenticationProviders             []AuthenticationProviderElement
+	CanCurrentUserEditIdentitiesForUser bool
+	Links                               map[string]string
 }
-
-// GetID returns the ID value of the user authentication.
-func (resource UserAuthentication) GetID() string {
-	return resource.ID
-}
-
-// GetLastModifiedBy returns the name of the account that modified the value of
-// this user authentication.
-func (resource UserAuthentication) GetLastModifiedBy() string {
-	return resource.LastModifiedBy
-}
-
-// GetLastModifiedOn returns the time when the value of this user
-// authentication was changed.
-func (resource UserAuthentication) GetLastModifiedOn() *time.Time {
-	return resource.LastModifiedOn
-}
-
-// GetLinks returns the associated links with the value of this user
-// authentication.
-func (resource UserAuthentication) GetLinks() map[string]string {
-	return resource.Links
-}
-
-// Validate checks the state of the user authentication and returns an error if
-// invalid.
-func (resource UserAuthentication) Validate() error {
-	validate := validator.New()
-	err := validate.Struct(resource)
-
-	if err != nil {
-		return err
-	}
-
-	return nil
-}
-
-var _ ResourceInterface = &UserAuthentication{}
