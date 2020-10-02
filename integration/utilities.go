da74b609f04602e51268734607322ea7499b9f6b
John Bristowe <john.bristowe@octopus.com>
2020-10-02 10:03:15 +1000

    Added common errors

--- integration/utilities.go
+++ integration/utilities.go
@@ -1,6 +1,9 @@
 package integration
 
-import "strings"
+import (
+	"fmt"
+	"strings"
+)
 
 const (
 	emptyString      string = ""
@@ -9,4 +12,32 @@ const (
 
 func isEmpty(s string) bool {
 	return len(strings.TrimSpace(s)) == 0
+}
+
+func createInvalidParameterError(methodName string, parameterName string) error {
+	return fmt.Errorf("%s: the input parameter (%s) is invalid", methodName, parameterName)
+}
+
+func createInvalidClientStateError(serviceName string) error {
+	return fmt.Errorf("%s: the state of the internal client is invalid", serviceName)
+}
+
+func createInvalidPathError(serviceName string) error {
+	return fmt.Errorf("%s: the internal path is not set", serviceName)
+}
+
+func createItemNotFoundError(serviceName string, methodName string, name string) error {
+	return fmt.Errorf("%s: the item (%s) via %s was not found", serviceName, name, methodName)
+}
+
+func createClientInitializationError(methodName string) error {
+	return fmt.Errorf("%s: unable to initialize internal client", methodName)
+}
+
+func createResourceNotFoundError(name string, identifier string, value string) error {
+	return fmt.Errorf("%s %s (%s) could not be found", name, identifier, value)
+}
+
+func createValidationFailureError(methodName string, err error) error {
+	return fmt.Errorf("validation failure in %s; %v", methodName, err)
 }
