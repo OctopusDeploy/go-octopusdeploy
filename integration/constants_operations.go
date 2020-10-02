2948c14e3e3610cd8ffb9cd32848d48922cfb540
John Bristowe <john.bristowe@octopus.com>
2020-10-02 10:03:05 +1000

    Added shared constants

---
+++ integration/constants_operations.go
@@ -0,0 +1,21 @@
+package integration
+
+const (
+	operationAdd              = "Add"
+	operationAPIAdd           = "apiAdd"
+	operationAPIDelete        = "apiDelete"
+	operationAPIGet           = "apiGet"
+	operationAPIPost          = "apiPost"
+	operationAPIUpdate        = "apiUpdate"
+	operationDeleteByID       = "DeleteByID"
+	operationGet              = "Get"
+	operationGetByID          = "GetByID"
+	operationGetByIDs         = "GetByIDs"
+	operationGetByName        = "GetByName"
+	operationGetByPartialName = "GetByPartialName"
+	operationGetByProjectID   = "GetByProjectID"
+	operationGetByUserID      = "GetByUserID"
+	operationInstall          = "Install"
+	operationReplace          = "Replace"
+	operationUpdate           = "Update"
+)
