30be2954035d3d6e90fccab8dd3034c389f53e72
John Bristowe <john.bristowe@octopus.com>
2020-10-19 11:48:36 +1000

    Moved test helpers

--- integration/test_helpers.go
+++ integration/test_helpers.go
@@ -1,16 +1,13 @@
 package integration
 
 import (
-	"fmt"
 	"log"
 	"net/http"
 	"net/url"
 	"os"
-	"time"
 
 	"github.com/OctopusDeploy/go-octopusdeploy/client"
 	"github.com/OctopusDeploy/go-octopusdeploy/model"
-	uuid "github.com/google/uuid"
 )
 
 func getOctopusClient() *client.Client {
@@ -42,16 +39,6 @@ func getOctopusClient() *client.Client {
 	}
 
 	return octopusClient
-}
-
-func getRandomName() string {
-	fullName := fmt.Sprintf("go-octopusdeploy %s", uuid.New())
-	fullName = fullName[0:49] //Some names in Octopus have a max limit of 50 characters (such as Environment Name)
-	return fullName
-}
-
-func getRandomVarName() string {
-	return fmt.Sprintf("go-octo-%v", time.Now().Unix())
 }
 
 func generateSensitiveValue() model.SensitiveValue {
