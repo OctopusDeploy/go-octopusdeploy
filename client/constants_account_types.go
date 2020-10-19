28f8ae9c9a0acc573ca167d0195ca8e9ffe8bf4c
John Bristowe <john.bristowe@octopus.com>
2020-10-19 22:13:43 +1000

    Added account types

---
+++ client/constants_account_types.go
@@ -0,0 +1,10 @@
+package client
+
+const (
+	accountTypeAmazonWebServicesAccount string = "AmazonWebServicesAccount"
+	accountTypeAzureServicePrincipal    string = "AzureServicePrincipal"
+	accountTypeAzureSubscription        string = "AzureSubscription"
+	accountTypeSshKeyPair               string = "SshKeyPair"
+	accountTypeToken                    string = "Token"
+	accountTypeUsernamePassword         string = "UsernamePassword"
+)
