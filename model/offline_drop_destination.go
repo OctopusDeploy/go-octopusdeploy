7144d6769fdc5a51d575db47d5fffa6499210a69
John Bristowe <john.bristowe@octopus.com>
2020-10-19 16:18:09 +1000

    Added New function

--- model/offline_drop_destination.go
+++ model/offline_drop_destination.go
@@ -4,3 +4,7 @@ type OfflineDropDestination struct {
 	DestinationType string `json:"DestinationType validate="oneof=Artifact FileSystem"`
 	DropFolderPath  string `json:"DropFolderPath,omitempty"`
 }
+
+func NewOfflineDropDestination() *OfflineDropDestination {
+	return &OfflineDropDestination{}
+}
