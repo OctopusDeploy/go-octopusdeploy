00aefae2de9e399e229a2d6b23be679707df44d2
John Bristowe <john.bristowe@octopus.com>
2020-10-19 22:23:44 +1000

    Added channel tests

---
+++ integration/channel_service_test.go
@@ -0,0 +1,74 @@
+package integration
+
+import (
+	"testing"
+
+	"github.com/OctopusDeploy/go-octopusdeploy/model"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestChannelServiceAdd(t *testing.T) {
+	octopusClient := getOctopusClient()
+	require.NotNil(t, octopusClient)
+
+	emptyChannel := &model.Channel{}
+	channel, err := octopusClient.Channels.Add(emptyChannel)
+	assert.Equal(t, createValidationFailureError("Add", emptyChannel.Validate()), err)
+	assert.Nil(t, channel)
+}
+
+func TestChannelServiceGetAll(t *testing.T) {
+	octopusClient := getOctopusClient()
+	require.NotNil(t, octopusClient)
+
+	channels, err := octopusClient.Channels.GetAll()
+	assert.NoError(t, err)
+	assert.NotNil(t, channels)
+
+	for _, channel := range channels {
+		assert.NotNil(t, channel)
+		assert.NotEmpty(t, channel.ID)
+	}
+}
+
+func TestChannelServiceGetByID(t *testing.T) {
+	octopusClient := getOctopusClient()
+	require.NotNil(t, octopusClient)
+
+	id := getRandomName()
+	channel, err := octopusClient.Channels.GetByID(id)
+	assert.Equal(t, createResourceNotFoundError("ChannelService", "ID", id), err)
+	assert.Nil(t, channel)
+
+	channels, err := octopusClient.Channels.GetAll()
+	assert.NoError(t, err)
+	assert.NotNil(t, channels)
+
+	for _, channel := range channels {
+		channelToCompare, err := octopusClient.Channels.GetByID(channel.ID)
+		assert.NoError(t, err)
+		assert.EqualValues(t, channel, channelToCompare)
+	}
+}
+
+func TestChannelServiceGetByPartialName(t *testing.T) {
+	octopusClient := getOctopusClient()
+	require.NotNil(t, octopusClient)
+
+	name := getRandomName()
+	channels, err := octopusClient.Channels.GetByPartialName(name)
+	assert.NoError(t, err)
+	assert.NotNil(t, channels)
+	assert.Len(t, channels, 0)
+
+	channels, err = octopusClient.Channels.GetAll()
+	assert.NoError(t, err)
+	assert.NotNil(t, channels)
+
+	for _, channel := range channels {
+		channelToCompare, err := octopusClient.Channels.GetByPartialName(channel.Name)
+		assert.NoError(t, err)
+		assert.EqualValues(t, channel, channelToCompare)
+	}
+}
