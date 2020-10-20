ab52db9b53e1442f9815edf29754b76ed84a660d
John Bristowe <john.bristowe@octopus.com>
2020-10-21 07:13:00 +1000

    Defined unit tests

--- octopusdeploy/deployment_process_service_test.go
+++ octopusdeploy/deployment_process_service_test.go
@@ -5,13 +5,20 @@ import (
 
 	"github.com/dghubble/sling"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
+
+func createDeploymentProcessService(t *testing.T) *deploymentProcessService {
+	service := newDeploymentProcessService(nil, TestURIDeploymentProcesses)
+	testNewService(t, service, TestURIDeploymentProcesses, serviceDeploymentProcessesService)
+	return service
+}
 
 func TestNewDeploymentProcessService(t *testing.T) {
 	serviceFunction := newDeploymentProcessService
 	client := &sling.Sling{}
 	uriTemplate := emptyString
-	serviceName := serviceDeploymentProcesseService
+	serviceName := serviceDeploymentProcessesService
 
 	testCases := []struct {
 		name        string
@@ -27,6 +34,33 @@ func TestNewDeploymentProcessService(t *testing.T) {
 		t.Run(tc.name, func(t *testing.T) {
 			service := tc.f(tc.client, tc.uriTemplate)
 			testNewService(t, service, uriTemplate, serviceName)
+		})
+	}
+}
+
+func TestDeploymentProcessServiceParameters(t *testing.T) {
+	testCases := []struct {
+		name      string
+		parameter string
+	}{
+		{"Empty", emptyString},
+		{"Whitespace", whitespaceString},
+		{"InvalidID", getRandomName()},
+	}
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			service := createDeploymentProcessService(t)
+			require.NotNil(t, service)
+
+			if isEmpty(tc.parameter) {
+				resource, err := service.GetByID(tc.parameter)
+				require.Equal(t, err, createInvalidParameterError(operationGetByID, parameterID))
+				require.Nil(t, resource)
+			} else {
+				resource, err := service.GetByID(tc.parameter)
+				require.Equal(t, err, createResourceNotFoundError(serviceDeploymentProcessesService, "ID", tc.parameter))
+				require.Nil(t, resource)
+			}
 		})
 	}
 }
