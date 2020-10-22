a6973d71688d8c8041243ab04eb6addd6b02e9f6
John Bristowe <john.bristowe@octopus.com>
2020-10-22 15:47:48 +1000

    Added unit tests for NotAll validator

---
+++ octopusdeploy/not_all_validator_test.go
@@ -0,0 +1,41 @@
+package octopusdeploy
+
+import (
+	"testing"
+
+	"github.com/go-playground/validator/v10"
+	"github.com/stretchr/testify/require"
+)
+
+type NotAllTestStruct struct {
+	Name string `validate:"notall"`
+}
+
+func TestNotAllValidation(t *testing.T) {
+	notAll := &NotAllTestStruct{
+		Name: "All",
+	}
+
+	v := validator.New()
+
+	err := v.RegisterValidation("notall", NotAll)
+	require.NoError(t, err)
+
+	err = v.Struct(notAll)
+	require.Error(t, err)
+
+	notAll.Name = "all"
+
+	err = v.Struct(notAll)
+	require.Error(t, err)
+
+	notAll.Name = "ALL"
+
+	err = v.Struct(notAll)
+	require.Error(t, err)
+
+	notAll.Name = "aLl"
+
+	err = v.Struct(notAll)
+	require.Error(t, err)
+}
