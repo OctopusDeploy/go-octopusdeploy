cd8e0997f0c9a4e6ba781c4d52b407794a222257
John Bristowe <john.bristowe@octopus.com>
2020-10-22 14:03:21 +1000

    Moved and updated tests for Teams

---
+++ integration/team_service_test.go
@@ -0,0 +1,161 @@
+package integration
+
+import (
+	"testing"
+
+	"github.com/OctopusDeploy/go-octopusdeploy/octopusdeploy"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func CreateTestTeam(t *testing.T, client *octopusdeploy.Client) *octopusdeploy.Team {
+	if client == nil {
+		client = getOctopusClient()
+	}
+	require.NotNil(t, client)
+
+	name := getRandomName()
+
+	team := octopusdeploy.NewTeam(name)
+	require.NoError(t, team.Validate())
+
+	createdTeam, err := client.Teams.Add(team)
+	require.NoError(t, err)
+	require.NotNil(t, createdTeam)
+	require.NotEmpty(t, createdTeam.GetID())
+	require.Equal(t, name, createdTeam.Name)
+
+	return createdTeam
+}
+
+func DeleteTestTeam(t *testing.T, client *octopusdeploy.Client, team *octopusdeploy.Team) {
+	require.NotNil(t, team)
+
+	if client == nil {
+		client = getOctopusClient()
+	}
+	require.NotNil(t, client)
+
+	err := client.Teams.DeleteByID(team.GetID())
+	assert.NoError(t, err)
+
+	// verify the delete operation was successful
+	teams, err := client.Teams.GetByID(team.GetID())
+	assert.Error(t, err)
+	assert.Nil(t, teams)
+}
+
+func IsEqualTeams(t *testing.T, expected *octopusdeploy.Team, actual *octopusdeploy.Team) {
+	// equality cannot be determined through a direct comparison (below)
+	// because APIs like GetByPartialName do not include the fields,
+	// LastModifiedBy and LastModifiedOn
+	//
+	// assert.EqualValues(expected, actual)
+	//
+	// this statement (above) is expected to succeed, but it fails due to these
+	// missing fields
+
+	// IResource
+	assert.Equal(t, expected.GetID(), actual.GetID())
+	assert.True(t, IsEqualLinks(expected.GetLinks(), actual.GetLinks()))
+
+	// team
+	assert.Equal(t, expected.CanBeDeleted, actual.CanBeDeleted)
+	assert.Equal(t, expected.CanBeRenamed, actual.CanBeRenamed)
+	assert.Equal(t, expected.CanChangeMembers, actual.CanChangeMembers)
+	assert.Equal(t, expected.CanChangeRoles, actual.CanChangeRoles)
+	assert.Equal(t, expected.Description, actual.Description)
+	assert.Equal(t, expected.ExternalSecurityGroups, actual.ExternalSecurityGroups)
+	assert.Equal(t, expected.MemberUserIDs, actual.MemberUserIDs)
+	assert.Equal(t, expected.Name, actual.Name)
+	assert.Equal(t, expected.SpaceID, actual.SpaceID)
+}
+
+func UpdateTeam(t *testing.T, client *octopusdeploy.Client, team *octopusdeploy.Team) *octopusdeploy.Team {
+	require.NotNil(t, team)
+
+	if client == nil {
+		client = getOctopusClient()
+	}
+	require.NotNil(t, client)
+
+	updatedTeam, err := client.Teams.Update(team)
+	require.NoError(t, err)
+	require.NotNil(t, updatedTeam)
+
+	return updatedTeam
+}
+
+func TestTeamSetAddGetDelete(t *testing.T) {
+	client := getOctopusClient()
+	require.NotNil(t, client)
+
+	team := CreateTestTeam(t, client)
+	require.NotNil(t, team)
+	defer DeleteTestTeam(t, client, team)
+
+	teamToCompare, err := client.Teams.GetByID(team.GetID())
+	require.NoError(t, err)
+	require.NotNil(t, teamToCompare)
+	IsEqualTeams(t, team, teamToCompare)
+}
+
+func TestTeamServiceDeleteAll(t *testing.T) {
+	client := getOctopusClient()
+	require.NotNil(t, client)
+
+	teams, err := client.Teams.GetAll()
+	require.NoError(t, err)
+	require.NotNil(t, teams)
+
+	for _, team := range teams {
+		defer DeleteTestTeam(t, client, team)
+	}
+}
+
+func TestTeamServiceGetAll(t *testing.T) {
+	client := getOctopusClient()
+	require.NotNil(t, client)
+
+	// create 30 test teams (to be deleted)
+	for i := 0; i < 30; i++ {
+		team := CreateTestTeam(t, client)
+		require.NotNil(t, team)
+		defer DeleteTestTeam(t, client, team)
+	}
+
+	allTeams, err := client.Teams.GetAll()
+	require.NoError(t, err)
+	require.NotNil(t, allTeams)
+	require.True(t, len(allTeams) >= 30)
+}
+
+func TestTeamServiceGetByID(t *testing.T) {
+	client := getOctopusClient()
+	require.NotNil(t, client)
+
+	id := getRandomName()
+	team, err := client.Teams.GetByID(id)
+	require.Equal(t, createResourceNotFoundError(serviceTeamService, "ID", id), err)
+	require.Nil(t, team)
+
+	teams, err := client.Teams.GetAll()
+	require.NoError(t, err)
+	require.NotNil(t, teams)
+
+	for _, team := range teams {
+		teamToCompare, err := client.Teams.GetByID(team.GetID())
+		require.NoError(t, err)
+		IsEqualTeams(t, team, teamToCompare)
+	}
+}
+
+func TestTeamServiceUpdate(t *testing.T) {
+	client := getOctopusClient()
+	require.NotNil(t, client)
+
+	createdTeam := CreateTestTeam(t, client)
+	updatedTeam := UpdateTeam(t, client, createdTeam)
+	IsEqualTeams(t, createdTeam, updatedTeam)
+	defer DeleteTestTeam(t, client, updatedTeam)
+}
--- octopusdeploy/team_service_test.go
+++ octopusdeploy/team_service_test.go
@@ -4,7 +4,6 @@ import (
 	"testing"
 
 	"github.com/dghubble/sling"
-	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
 
@@ -12,79 +11,6 @@ func createTeamService(t *testing.T) *teamService {
 	service := newTeamService(nil, TestURITeams)
 	testNewService(t, service, TestURITeams, serviceTeamService)
 	return service
-}
-
-func CreateTestTeam(t *testing.T, service *teamService) *Team {
-	if service == nil {
-		service = createTeamService(t)
-	}
-	require.NotNil(t, service)
-
-	name := getRandomName()
-
-	team := NewTeam(name)
-	require.NoError(t, team.Validate())
-
-	createdTeam, err := service.Add(team)
-	require.NoError(t, err)
-	require.NotNil(t, createdTeam)
-	require.NotEmpty(t, createdTeam.GetID())
-	require.Equal(t, name, createdTeam.Name)
-
-	return createdTeam
-}
-
-func DeleteTestTeam(t *testing.T, service *teamService, team *Team) error {
-	require.NotNil(t, team)
-
-	if service == nil {
-		service = createTeamService(t)
-	}
-	require.NotNil(t, service)
-
-	err := service.DeleteByID(team.GetID())
-	assert.NoError(t, err)
-
-	return err
-}
-
-func IsEqualTeams(t *testing.T, expected *Team, actual *Team) {
-	// equality cannot be determined through a direct comparison (below)
-	// because APIs like GetByPartialName do not include the fields,
-	// LastModifiedBy and LastModifiedOn
-	//
-	// assert.EqualValues(expected, actual)
-	//
-	// this statement (above) is expected to succeed, but it fails due to these
-	// missing fields
-
-	// IResource
-	assert.Equal(t, expected.GetID(), actual.GetID())
-	assert.True(t, IsEqualLinks(expected.GetLinks(), actual.GetLinks()))
-
-	// team
-	assert.Equal(t, expected.CanBeDeleted, actual.CanBeDeleted)
-	assert.Equal(t, expected.CanBeRenamed, actual.CanBeRenamed)
-	assert.Equal(t, expected.CanChangeMembers, actual.CanChangeMembers)
-	assert.Equal(t, expected.CanChangeRoles, actual.CanChangeRoles)
-	assert.Equal(t, expected.Description, actual.Description)
-	assert.Equal(t, expected.ExternalSecurityGroups, actual.ExternalSecurityGroups)
-	assert.Equal(t, expected.MemberUserIDs, actual.MemberUserIDs)
-	assert.Equal(t, expected.Name, actual.Name)
-	assert.Equal(t, expected.SpaceID, actual.SpaceID)
-}
-
-func UpdateTeam(t *testing.T, service *teamService, team *Team) *Team {
-	if service == nil {
-		service = createTeamService(t)
-	}
-	require.NotNil(t, service)
-
-	updatedTeam, err := service.Update(team)
-	require.NoError(t, err)
-	require.NotNil(t, updatedTeam)
-
-	return updatedTeam
 }
 
 func TestTeamSetAddGetDelete(t *testing.T) {
@@ -98,15 +24,6 @@ func TestTeamSetAddGetDelete(t *testing.T) {
 	resource, err = service.Add(&Team{})
 	require.Error(t, err)
 	require.Nil(t, resource)
-
-	resource = CreateTestTeam(t, service)
-	require.NotNil(t, resource)
-	defer DeleteTestTeam(t, service, resource)
-
-	resourceToCompare, err := service.GetByID(resource.GetID())
-	require.NoError(t, err)
-	require.NotNil(t, resourceToCompare)
-	IsEqualTeams(t, resource, resourceToCompare)
 }
 
 func TestTeamServiceAdd(t *testing.T) {
@@ -120,70 +37,6 @@ func TestTeamServiceAdd(t *testing.T) {
 	resource, err = service.Add(&Team{})
 	require.Error(t, err)
 	require.Nil(t, resource)
-
-	resource = CreateTestTeam(t, service)
-	err = DeleteTestTeam(t, service, resource)
-	require.NoError(t, err)
-}
-
-func TestTeamServiceDeleteAll(t *testing.T) {
-	service := createTeamService(t)
-	require.NotNil(t, service)
-
-	resources, err := service.GetAll()
-	require.NoError(t, err)
-	require.NotNil(t, resources)
-
-	for _, resource := range resources {
-		err = DeleteTestTeam(t, service, resource)
-		require.NoError(t, err)
-	}
-}
-
-func TestTeamServiceGetAll(t *testing.T) {
-	service := createTeamService(t)
-	require.NotNil(t, service)
-
-	teams := []Team{}
-
-	// create 30 test teams (to be deleted)
-	for i := 0; i < 30; i++ {
-		team := CreateTestTeam(t, service)
-		require.NotNil(t, team)
-		teams = append(teams, *team)
-	}
-
-	allTeams, err := service.GetAll()
-	require.NoError(t, err)
-	require.NotNil(t, allTeams)
-	require.True(t, len(allTeams) >= 30)
-
-	for _, team := range teams {
-		require.NotNil(t, team)
-		require.NotEmpty(t, team.GetID())
-		err = DeleteTestTeam(t, service, &team)
-		require.NoError(t, err)
-	}
-}
-
-func TestTeamServiceGetByID(t *testing.T) {
-	service := createTeamService(t)
-	require.NotNil(t, service)
-
-	id := getRandomName()
-	resource, err := service.GetByID(id)
-	require.Equal(t, createResourceNotFoundError(service.getName(), "ID", id), err)
-	require.Nil(t, resource)
-
-	resources, err := service.GetAll()
-	require.NoError(t, err)
-	require.NotNil(t, resources)
-
-	for _, resource := range resources {
-		resourceToCompare, err := service.GetByID(resource.GetID())
-		require.NoError(t, err)
-		IsEqualTeams(t, resource, resourceToCompare)
-	}
 }
 
 func TestTeamServiceParameters(t *testing.T) {
@@ -247,47 +100,4 @@ func TestTeamServiceNew(t *testing.T) {
 			testNewService(t, service, uriTemplate, serviceName)
 		})
 	}
-}
-
-func TestTeamGetByPartialName(t *testing.T) {
-	service := createTeamService(t)
-	require.NotNil(t, service)
-
-	resources, err := service.GetByPartialName(emptyString)
-	require.Equal(t, err, createInvalidParameterError(operationGetByPartialName, parameterName))
-	require.NotNil(t, resources)
-	require.Len(t, resources, 0)
-
-	resources, err = service.GetByPartialName(whitespaceString)
-	require.Equal(t, err, createInvalidParameterError(operationGetByPartialName, parameterName))
-	require.NotNil(t, resources)
-	require.Len(t, resources, 0)
-
-	resources, err = service.GetAll()
-	require.NoError(t, err)
-	require.NotNil(t, resources)
-
-	for _, resource := range resources {
-		namedResources, err := service.GetByPartialName(resource.Name)
-		require.NoError(t, err)
-		require.NotNil(t, namedResources)
-	}
-}
-
-func TestTeamServiceUpdate(t *testing.T) {
-	service := createTeamService(t)
-	require.NotNil(t, service)
-
-	resource, err := service.Update(nil)
-	require.Error(t, err)
-	require.Nil(t, resource)
-
-	resource, err = service.Update(&Team{})
-	require.Error(t, err)
-	require.Nil(t, resource)
-
-	expected := CreateTestTeam(t, service)
-	actual := UpdateTeam(t, service, expected)
-	IsEqualTeams(t, expected, actual)
-	defer DeleteTestTeam(t, service, expected)
 }
