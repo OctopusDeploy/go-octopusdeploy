b8ccf6830f751de54ec5a64bcc6e15ff535fe57b
John Bristowe <john.bristowe@octopus.com>
2020-10-27 16:22:07 +1000

    Defined AccountType

---
+++ octopusdeploy/account_types.go
@@ -0,0 +1,13 @@
+package octopusdeploy
+
+type AccountType string
+
+const (
+	AccountTypeNone                     AccountType = "None"
+	AccountTypeAmazonWebServicesAccount AccountType = "AmazonWebServicesAccount"
+	AccountTypeAzureServicePrincipal    AccountType = "AzureServicePrincipal"
+	AccountTypeAzureSubscription        AccountType = "AzureSubscription"
+	AccountTypeSSHKeyPair               AccountType = "SshKeyPair"
+	AccountTypeToken                    AccountType = "Token"
+	AccountTypeUsernamePassword         AccountType = "UsernamePassword"
+)
