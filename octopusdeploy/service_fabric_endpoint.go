1adfabdf788506a2d75f902e7f63f0eb697299a2
John Bristowe <john.bristowe@octopus.com>
2020-11-14 06:24:29 +1000

    Reordered fields

--- octopusdeploy/service_fabric_endpoint.go
+++ octopusdeploy/service_fabric_endpoint.go
@@ -5,8 +5,8 @@ import "github.com/go-playground/validator/v10"
 type AzureServiceFabricEndpoint struct {
 	AadClientCredentialSecret   string          `json:"AadClientCredentialSecret,omitempty"`
 	AadCredentialType           string          `json:"AadCredentialType,omitempty" validate:"omitempty,oneof=ClientCredential UserCredential"`
-	AadUserCredentialUsername   string          `json:"AadUserCredentialUsername,omitempty"`
 	AadUserCredentialPassword   *SensitiveValue `json:"AadUserCredentialPassword,omitempty"`
+	AadUserCredentialUsername   string          `json:"AadUserCredentialUsername,omitempty"`
 	CertificateStoreLocation    string          `json:"CertificateStoreLocation,omitempty"`
 	CertificateStoreName        string          `json:"CertificateStoreName,omitempty"`
 	ClientCertificateVariable   string          `json:"ClientCertVariable,omitempty"`
