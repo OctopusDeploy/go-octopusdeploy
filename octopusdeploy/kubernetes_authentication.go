d359977c8f1838f14b199507232486dec12e6d22
John Bristowe <john.bristowe@octopus.com>
2020-11-19 09:33:12 +1000

    Reordered fields (formatting)

--- octopusdeploy/kubernetes_authentication.go
+++ octopusdeploy/kubernetes_authentication.go
@@ -3,9 +3,9 @@ package octopusdeploy
 type KubernetesAuthentication struct {
 	AccountID                 string `json:"AccountId,omitempty"`
 	AdminLogin                string `json:"AdminLogin,omitempty"`
-	AssumeRole                bool   `json:"AssumeRole,omitempty"`
 	AssumedRoleARN            string `json:"AssumedRoleArn,omitempty"`
 	AssumedRoleSession        string `json:"AssumedRoleSession,omitempty"`
+	AssumeRole                bool   `json:"AssumeRole,omitempty"`
 	AssumeRoleSessionDuration int    `json:"AssumeRoleSessionDurationSeconds,omitempty"`
 	AssumeRoleExternalID      string `json:"AssumeRoleExternalId,omitempty"`
 	AuthenticationType        string `json:"AuthenticationType,omitempty"`
