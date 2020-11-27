5906b20b8a774e9b8adaf44ee40e7cb341d6209c
jbristowe <john.bristowe@octopus.com>
2020-11-27 16:10:47 +1000

    Added user role and its service
    Fixes #42 and #41

---
+++ octopusdeploy/user_role.go
@@ -0,0 +1,22 @@
+package octopusdeploy
+
+// UserRoles defines a collection of user roles with built-in support for paged
+// results.
+type UserRoles struct {
+	Items []*UserRole `json:"Items"`
+	PagedResults
+}
+
+// UserRole represents a user role in Octopus.
+type UserRole struct {
+	CanBeDeleted                 bool     `json:"CanBeDeleted,omitempty"`
+	Description                  string   `json:"Description,omitempty"`
+	GrantedSpacePermissions      []string `json:"GrantedSpacePermissions"`
+	GrantedSystemPermissions     []string `json:"GrantedSystemPermissions"`
+	Name                         string   `json:"Name,omitempty"`
+	SpacePermissionDescriptions  []string `json:"SpacePermissionDescriptions"`
+	SupportedRestrictions        []string `json:"SupportedRestrictions"`
+	SystemPermissionDescriptions []string `json:"SystemPermissionDescriptions"`
+
+	resource
+}
--- octopusdeploy/user_role_service.go
+++ octopusdeploy/user_role_service.go
@@ -12,3 +12,20 @@ func newUserRoleService(sling *sling.Sling, uriTemplate string) *userRoleService
 
 	return userRoleService
 }
+
+// Get returns a collection of user roles based on the criteria defined by its
+// input query parameter. If an error occurs, an empty collection is returned
+// along with the associated error.
+func (s userRoleService) Get(userRolesQuery UserRolesQuery) (*UserRoles, error) {
+	path, err := s.getURITemplate().Expand(userRolesQuery)
+	if err != nil {
+		return &UserRoles{}, err
+	}
+
+	response, err := apiGet(s.getClient(), new(UserRoles), path)
+	if err != nil {
+		return &UserRoles{}, err
+	}
+
+	return response.(*UserRoles), nil
+}
