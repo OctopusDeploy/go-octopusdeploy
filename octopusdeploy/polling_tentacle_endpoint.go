eca0f88399d475337b9fb9fb8cbedd878afbbc35
jbristowe <john.bristowe@octopus.com>
2020-12-01 09:41:25 +1000

    Implemented JSON marshalling for polling tentacle endpoint

--- octopusdeploy/polling_tentacle_endpoint.go
+++ octopusdeploy/polling_tentacle_endpoint.go
@@ -1,6 +1,11 @@
 package octopusdeploy
 
-import "net/url"
+import (
+	"encoding/json"
+	"net/url"
+
+	"github.com/go-playground/validator/v10"
+)
 
 type PollingTentacleEndpoint struct {
 	URI *url.URL `json:"Uri" validate:"required,uri"`
@@ -13,4 +18,52 @@ func NewPollingTentacleEndpoint(uri *url.URL, thumbprint string) *PollingTentacl
 		tentacleEndpoint: *newTentacleEndpoint("TentacleActive", thumbprint),
 		URI:              uri,
 	}
+}
+
+func (l PollingTentacleEndpoint) MarshalJSON() ([]byte, error) {
+	pollingTentacleEndpoint := struct {
+		URI string `json:"Uri" validate:"required,uri"`
+		tentacleEndpoint
+	}{
+		URI:              l.URI.String(),
+		tentacleEndpoint: l.tentacleEndpoint,
+	}
+
+	return json.Marshal(pollingTentacleEndpoint)
+}
+
+// UnmarshalJSON sets this polling tentacle endpoint to its representation in
+// JSON.
+func (l *PollingTentacleEndpoint) UnmarshalJSON(b []byte) error {
+	var fields struct {
+		URI string `json:"Uri" validate:"required,uri"`
+		tentacleEndpoint
+	}
+	err := json.Unmarshal(b, &fields)
+	if err != nil {
+		return err
+	}
+
+	// validate JSON representation
+	validate := validator.New()
+	err = validate.Struct(fields)
+	if err != nil {
+		return err
+	}
+
+	l.tentacleEndpoint = fields.tentacleEndpoint
+
+	u, err := url.Parse(fields.URI)
+	if err != nil {
+		return err
+	}
+	l.URI = u
+
+	return nil
+}
+
+// Validate checks the state of the listening tentacle endpoint and returns an
+// error if invalid.
+func (l *PollingTentacleEndpoint) Validate() error {
+	return validator.New().Struct(l)
 }
