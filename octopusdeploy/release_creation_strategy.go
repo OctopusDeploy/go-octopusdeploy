4fdcca10bdb7657110572ebc68847bcaa51240ab
jbristowe <john.bristowe@octopus.com>
2020-12-14 18:25:59 +1000

    Updated ReleaseCreationPackageStepID type

--- octopusdeploy/release_creation_strategy.go
+++ octopusdeploy/release_creation_strategy.go
@@ -3,5 +3,5 @@ package octopusdeploy
 type ReleaseCreationStrategy struct {
 	ChannelID                    string                   `json:"ChannelId,omitempty"`
 	ReleaseCreationPackage       *DeploymentActionPackage `json:"ReleaseCreationPackage,omitempty"`
-	ReleaseCreationPackageStepID *string                  `json:"ReleaseCreationPackageStepId,omitempty"`
+	ReleaseCreationPackageStepID string                   `json:"ReleaseCreationPackageStepId,omitempty"`
 }
