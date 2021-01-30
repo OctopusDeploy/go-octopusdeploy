a7a18967e75ca7d4aae7d847224e83dd57ac5025
jbristowe <john.bristowe@octopus.com>
2021-01-31 09:33:44 +1000

    Added missing properties for deployment action

--- octopusdeploy/deployment_action.go
+++ octopusdeploy/deployment_action.go
@@ -1,19 +1,24 @@
 package octopusdeploy
 
 type DeploymentAction struct {
-	ActionType                    string             `json:"ActionType"`
-	CanBeUsedForProjectVersioning bool               `json:"CanBeUsedForProjectVersioning"`
-	Channels                      []string           `json:"Channels,omitempty"`
-	Environments                  []string           `json:"Environments,omitempty"`
-	ExcludedEnvironments          []string           `json:"ExcludedEnvironments,omitempty"`
-	ID                            string             `json:"Id,omitempty"`
-	IsDisabled                    bool               `json:"IsDisabled"`
-	IsRequired                    bool               `json:"IsRequired"`
-	Name                          string             `json:"Name"`
-	Packages                      []PackageReference `json:"Packages,omitempty"`
-	Properties                    map[string]string  `json:"Properties"` // TODO: refactor to use the PropertyValueResource for handling sensitive values - https://blog.gopheracademy.com/advent-2016/advanced-encoding-decoding/
-	TenantTags                    []string           `json:"TenantTags,omitempty"`
-	WorkerPoolID                  string             `json:"WorkerPoolId,omitempty"`
+	ActionType                    string                    `json:"ActionType"`
+	CanBeUsedForProjectVersioning bool                      `json:"CanBeUsedForProjectVersioning"`
+	Channels                      []string                  `json:"Channels,omitempty"`
+	Condition                     string                    `json:"Condition,omitempty"`
+	Container                     DeploymentActionContainer `json:"Container"`
+	Environments                  []string                  `json:"Environments,omitempty"`
+	ExcludedEnvironments          []string                  `json:"ExcludedEnvironments,omitempty"`
+	ID                            string                    `json:"Id,omitempty"`
+	IsDisabled                    bool                      `json:"IsDisabled"`
+	IsRequired                    bool                      `json:"IsRequired"`
+	Links                         map[string]string         `json:"Links,omitempty"`
+	Name                          string                    `json:"Name"`
+	Notes                         string                    `json:"Notes,omitempty"`
+	Packages                      []PackageReference        `json:"Packages,omitempty"`
+	Properties                    map[string]string         `json:"Properties"` // TODO: refactor to use the PropertyValueResource for handling sensitive values - https://blog.gopheracademy.com/advent-2016/advanced-encoding-decoding/
+	TenantTags                    []string                  `json:"TenantTags,omitempty"`
+	WorkerPoolID                  string                    `json:"WorkerPoolId,omitempty"`
+	WorkerPoolVariable            string                    `json:"WorkerPoolVariable,omitempty"`
 }
 
 // NewDeploymentAction initializes a DeploymentAction with a name.
