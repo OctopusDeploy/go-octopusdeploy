2e3587a67183e5eb5d31e807d876e7b07d2967df
jbristowe <john.bristowe@octopus.com>
2021-03-12 11:05:37 +1000

    Added BasedPath and HasValue

--- octopusdeploy/version_control_settings.go
+++ octopusdeploy/version_control_settings.go
@@ -1,7 +1,9 @@
 package octopusdeploy
 
 type VersionControlSettings struct {
+	BasePath      string          `json:"BasePath,omitempty"`
 	DefaultBranch string          `json:"DefaultBranch,omitempty"`
+	HasValue      bool            `json:"HasValue,omitempty"`
 	Password      *SensitiveValue `json:"Password,omitempty"`
 	URL           string          `json:"Url,omitempty"`
 	Username      string          `json:"Username,omitempty"`
