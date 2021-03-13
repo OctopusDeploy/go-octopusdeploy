5c1fd967ca3d526e66641cb72b96c9be81948fa9
jbristowe <john.bristowe@octopus.com>
2021-03-13 13:10:54 +1000

    Reordered properties

--- octopusdeploy/package_reference.go
+++ octopusdeploy/package_reference.go
@@ -1,10 +1,10 @@
 package octopusdeploy
 
 type PackageReference struct {
+	AcquisitionLocation string            `json:"AcquisitionLocation"` // This can be an expression
+	FeedID              string            `json:"FeedId"`
 	ID                  string            `json:"Id,omitempty"`
 	Name                string            `json:"Name,omitempty"`
 	PackageID           string            `json:"PackageId,omitempty"`
-	FeedID              string            `json:"FeedId"`
-	AcquisitionLocation string            `json:"AcquisitionLocation"` // This can be an expression
 	Properties          map[string]string `json:"Properties"`
 }
