d1487a67ce37d82b1018bf22d94974426c6bf826
Domenic Simone <domenic.simone@octopus.com>
2022-02-16 11:24:17 +1100

    fixed factory for account service

--- service/accountV1/account_service.go
+++ service/accountV1/account_service.go
@@ -1,9 +1,6 @@
 package accountV1
 
 import (
-	"github.com/OctopusDeploy/go-octopusdeploy/internal"
-
-	"github.com/OctopusDeploy/go-octopusdeploy/octopusdeploy"
 	"github.com/OctopusDeploy/go-octopusdeploy/service"
 )
 
@@ -16,7 +13,7 @@ type accountService struct {
 	service.CanAddService[IAccount]
 	service.CanUpdateService[IAccount]
 	service.CanDeleteService[IAccount]
-	service.CanQuery[IAccount, service.AccountsQuery]
+	service.CanQueryService[IAccount, service.AccountsQuery]
 	service.ISpaceScopedService
 }
 
@@ -31,38 +28,54 @@ type IAccountService interface {
 
 // NewAccountService returns an accountV1 service with a preconfigured client.
 func NewAccountService(client service.ISpaceScopedClient) IAccountService {
+	baseService := service.NewSpaceScopedService(service.ServiceAccountService, accountsV1BasePath, client)
 	accountService := &accountService{
-		ISpaceScopedService: service.NewSpaceScopedService(service.ServiceAccountService, accountsV1BasePath, client),
+		ISpaceScopedService: baseService,
+		CanGetByIDService: service.CanGetByIDService[IAccount]{
+			IService: baseService,
+		},
+		CanAddService: service.CanAddService[IAccount]{
+			IService: baseService,
+		},
+		CanUpdateService: service.CanUpdateService[IAccount]{
+			IService: baseService,
+		},
+		CanDeleteService: service.CanDeleteService[IAccount]{
+			IService: baseService,
+		},
+		CanQueryService: service.CanQueryService[IAccount, service.AccountsQuery]{
+			IService: baseService,
+		},
 	}
 	return accountService
 }
 
 // GetUsages lists the projects and deployments which are using an accountV1.
-func (s *accountService) GetUsages(account IAccount) (*AccountUsage, error) {
-	path := account.GetLinks()
-	resp, err := s.client.apiGet(new(AccountUsage), path)
-	if err != nil {
-		return nil, err
-	}
+// func (s *accountService) GetUsages(account IAccount) (*AccountUsage, error) {
+// 	path := account.GetLinks()
+// 	resp, err := s.client.apiGet(new(AccountUsage), path)
+// 	if err != nil {
+// 		return nil, err
+// 	}
 
-	return resp.(*AccountUsage), nil
-}
+// 	return resp.(*AccountUsage), nil
+// }
 
 // Update modifies an accountV1 based on the one provided as input.
-func (s *accountService) Update(account IAccount) (IAccount, error) {
-	if account == nil {
-		return nil, internal.CreateInvalidParameterError(service.OperationUpdate, octopusdeploy.ParameterAccount)
-	}
+// func (s *accountService) Update(account IAccount) (IAccount, error) {
+// 	if account == nil {
+// 		return nil, internal.CreateInvalidParameterError(service.OperationUpdate, octopusdeploy.ParameterAccount)
+// 	}
 
-	accountResource, err := ToAccountResource(s.client, account)
-	if err != nil {
-		return nil, err
-	}
+// 	accountResource, err := ToAccountResource(s.client, account)
+// 	if err != nil {
+// 		return nil, err
+// 	}
 
-	resp, err := s.client.apiUpdate(accountResource, new(AccountResource))
-	if err != nil {
-		return nil, err
-	}
+// 	resp, err := s.client.apiUpdate(accountResource, new(AccountResource))
+// 	if err != nil {
+// 		return nil, err
+// 	}
 
-	return ToAccount(resp.(*AccountResource))
-}
+// 	return ToAccount(resp.(*AccountResource))
+// }
