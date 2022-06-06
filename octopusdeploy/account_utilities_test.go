4710f4fe17c851f749ebf766b661f88bd589de0d
John Bristowe <john.bristowe@octopus.com>
2022-06-06 14:47:27 +1000

    chore: added test for ToAccountResource

--- octopusdeploy/account_utilities_test.go
+++ octopusdeploy/account_utilities_test.go
@@ -33,3 +33,45 @@ func TestToAccount(t *testing.T) {
 	require.NoError(t, err)
 	require.EqualValues(t, account.GetAccountType(), "UsernamePassword")
 }
+
+func TestToAccountResource(t *testing.T) {
+	var account IAccount
+	accountResource, err := ToAccountResource(account)
+	require.Nil(t, accountResource)
+	require.Error(t, err)
+
+	var amazonWebServicesAccount *AmazonWebServicesAccount
+	accountResource, err = ToAccountResource(amazonWebServicesAccount)
+	require.Nil(t, accountResource)
+	require.Error(t, err)
+
+	var azureServicePrincipalAccount *AzureServicePrincipalAccount
+	accountResource, err = ToAccountResource(azureServicePrincipalAccount)
+	require.Nil(t, accountResource)
+	require.Error(t, err)
+
+	var azureSubscriptionAccount *AzureSubscriptionAccount
+	accountResource, err = ToAccountResource(azureSubscriptionAccount)
+	require.Nil(t, accountResource)
+	require.Error(t, err)
+
+	var googleCloudPlatformAccount *GoogleCloudPlatformAccount
+	accountResource, err = ToAccountResource(googleCloudPlatformAccount)
+	require.Nil(t, accountResource)
+	require.Error(t, err)
+
+	var sshKeyAccount *SSHKeyAccount
+	accountResource, err = ToAccountResource(sshKeyAccount)
+	require.Nil(t, accountResource)
+	require.Error(t, err)
+
+	var tokenAccount *TokenAccount
+	accountResource, err = ToAccountResource(tokenAccount)
+	require.Nil(t, accountResource)
+	require.Error(t, err)
+
+	var usernamePasswordAccount *UsernamePasswordAccount
+	accountResource, err = ToAccountResource(usernamePasswordAccount)
+	require.Nil(t, accountResource)
+	require.Error(t, err)
+}
