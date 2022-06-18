f0f990d96cc32399a084e989cf15123a9b226a9a
John Bristowe <john.bristowe@octopus.com>
2022-06-18 12:01:51 +1000

    refactor: updated to reflect changes to project structure

--- internal/validation.go
+++ internal/validation.go
@@ -1,4 +1,4 @@
-package octopusdeploy
+package internal
 
 import (
 	"fmt"
@@ -6,11 +6,6 @@ import (
 	"strings"
 
 	"github.com/google/uuid"
-)
-
-const (
-	empty = ""
-	tab   = "\t"
 )
 
 // ValidateStringInSlice checks if a string is in the given slice
@@ -26,8 +21,8 @@ func ValidateStringInSlice(str string, list []string) bool {
 
 // ValidatePropertyValues returns an error if the given string is not in a slice of strings
 func ValidatePropertyValues(propertyName string, propertyValue string, validValues []string) error {
-	if isEmpty(propertyName) {
-		return createInvalidParameterError("ValidatePropertyValues", "propertyName")
+	if IsEmpty(propertyName) {
+		return CreateInvalidParameterError("ValidatePropertyValues", "propertyName")
 	}
 
 	if ValidateStringInSlice(propertyValue, validValues) {
@@ -39,8 +34,8 @@ func ValidatePropertyValues(propertyName string, propertyValue string, validValu
 
 // ValidateRequiredPropertyValue returns an error if the property value is empty
 func ValidateRequiredPropertyValue(propertyName string, propertyValue string) error {
-	if isEmpty(propertyName) {
-		return createInvalidParameterError("ValidateRequiredPropertyValue", "propertyName")
+	if IsEmpty(propertyName) {
+		return CreateInvalidParameterError("ValidateRequiredPropertyValue", "propertyName")
 	}
 
 	if len(propertyValue) > 0 {
@@ -51,32 +46,16 @@ func ValidateRequiredPropertyValue(propertyName string, propertyValue string) er
 }
 
 func ValidateRequiredUUID(propertyName string, id *uuid.UUID) error {
-	if isEmpty(propertyName) {
-		return createInvalidParameterError("ValidateRequiredUUID", "propertyName")
+	if IsEmpty(propertyName) {
+		return CreateInvalidParameterError("ValidateRequiredUUID", "propertyName")
 	}
 
 	if id == nil {
-		return createInvalidParameterError("ValidateRequiredUUID", "id")
+		return CreateInvalidParameterError("ValidateRequiredUUID", "id")
 	}
 
 	if *id == uuid.Nil {
 		return fmt.Errorf("%s is a required property; its value is an empty UUID", propertyName)
-	}
-
-	return nil
-}
-
-func ValidateRequiredSensitiveValue(propertyName string, sensitiveValue *SensitiveValue) error {
-	if isEmpty(propertyName) {
-		return createInvalidParameterError("ValidateRequiredSensitiveValue", "propertyName")
-	}
-
-	if !sensitiveValue.HasValue {
-		return fmt.Errorf("%s is a required property; its underlying value is not set", propertyName)
-	}
-
-	if len(*sensitiveValue.NewValue) == 0 {
-		return fmt.Errorf("%s is a required property; its underlying value is not set", propertyName)
 	}
 
 	return nil
@@ -103,8 +82,8 @@ func ValidatePropertiesMatch(firstProperty, firstPropertyName, secondProperty, s
 }
 
 func ValidateSemanticVersion(propertyName string, version string) error {
-	if isEmpty(propertyName) {
-		return createInvalidParameterError("ValidateSemanticVersion", "propertyName")
+	if IsEmpty(propertyName) {
+		return CreateInvalidParameterError("ValidateSemanticVersion", "propertyName")
 	}
 
 	re := regexp.MustCompile(`^(?P<major>0|[1-9]\d*)\.(?P<minor>0|[1-9]\d*)\.(?P<patch>0|[1-9]\d*)(?:-(?P<prerelease>(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*)(?:\.(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*))?(?:\+(?P<buildmetadata>[0-9a-zA-Z-]+(?:\.[0-9a-zA-Z-]+)*))?$`)
