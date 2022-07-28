389fdfe1a88022ce56026bc75fcd19aa7f4e3908
John Bristowe <john.bristowe@octopus.com>
2022-07-28 13:39:08 +1000

    fix: updated resource to reflect vLatest API

--- pkg/releases/create_release_v1.go
+++ pkg/releases/create_release_v1.go
@@ -1,22 +1,23 @@
 package releases
 
+import "encoding/json"
+
 type ICreateReleaseV1 interface {
 }
 
 type CreateReleaseV1 struct {
-	SpaceID               string   `json:"spaceId"`
-	SpaceIdOrName         string   `json:"spaceIdOrName"`
-	ProjectIdOrName       string   `json:"projectIdOrName"`
-	PackageVersion        string   `json:"packageVersion,omitempty"`
+	ChannelNameOrID       string   `json:"channelIdOrName,omitempty"`
+	IgnoreChannelRules    bool     `json:"ignoreChannelRules,omitempty"`
+	IgnoreIfAlreadyExists bool     `json:"ignoreIfAlreadyExists,omitempty"`
 	GitCommit             string   `json:"gitCommit,omitempty"`
 	GitRef                string   `json:"gitRef,omitempty"`
-	ReleaseVersion        string   `json:"releaseVersion,omitempty"`
-	ChannelName           string   `json:"channelIdOrName,omitempty"`
-	Packages              []string `json:"packages,omitempty"`
-	ReleaseNotes          string   `json:"releaseNotes,omitempty"`
-	IgnoreIfAlreadyExists bool     `json:"ignoreIfAlreadyExists,omitempty"`
-	IgnoreChannelRules    bool     `json:"ignoreChannelRules,omitempty"`
+	SpaceNameOrID         string   `json:"spaceIdOrName"`
 	PackagePrerelease     string   `json:"packagePrerelease,omitempty"`
+	Packages              []string `json:"packages,omitempty"`
+	PackageVersion        string   `json:"packageVersion,omitempty"`
+	ProjectNameOrID       string   `json:"projectName"`
+	ReleaseVersion        string   `json:"releaseVersion,omitempty"`
+	ReleaseNotes          string   `json:"releaseNotes,omitempty"`
 }
 
 type CreateReleaseResponseV1 struct {
@@ -25,10 +26,35 @@ type CreateReleaseResponseV1 struct {
 	AutomaticallyDeployedEnvironments string `json:"AutomaticallyDeployedEnvironments,omitempty"`
 }
 
-func NewCreateReleaseV1(spaceIdOrName string, projectIdOrName string) *CreateReleaseV1 {
+func NewCreateReleaseV1(spaceNameOrID string, projectNameOrID string) *CreateReleaseV1 {
 	return &CreateReleaseV1{
-		SpaceID:         spaceIdOrName,
-		SpaceIdOrName:   spaceIdOrName,
-		ProjectIdOrName: projectIdOrName,
+		SpaceNameOrID:   spaceNameOrID,
+		ProjectNameOrID: projectNameOrID,
 	}
+}
+
+// MarshalJSON returns create-release resource (V1) as its JSON encoding.
+func (c *CreateReleaseV1) MarshalJSON() ([]byte, error) {
+	createReleaseV1 := struct {
+		SpaceID string `json:"spaceId"`
+		CreateReleaseV1
+	}{
+		SpaceID: c.SpaceNameOrID,
+		CreateReleaseV1: CreateReleaseV1{
+			ChannelNameOrID:       c.ChannelNameOrID,
+			IgnoreChannelRules:    c.IgnoreChannelRules,
+			IgnoreIfAlreadyExists: c.IgnoreIfAlreadyExists,
+			GitCommit:             c.GitCommit,
+			GitRef:                c.GitRef,
+			SpaceNameOrID:         c.SpaceNameOrID,
+			PackagePrerelease:     c.PackagePrerelease,
+			Packages:              c.Packages,
+			PackageVersion:        c.PackageVersion,
+			ProjectNameOrID:       c.ProjectNameOrID,
+			ReleaseVersion:        c.ReleaseVersion,
+			ReleaseNotes:          c.ReleaseNotes,
+		},
+	}
+
+	return json.Marshal(createReleaseV1)
 }
