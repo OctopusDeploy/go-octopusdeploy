95d9e0c17dd9c4a7514590e2604404071ce05c4c
John Bristowe <john.bristowe@octopus.com>
2022-08-16 11:29:13 +1000

    feat: added new function for phases

--- pkg/lifecycles/phase.go
+++ pkg/lifecycles/phase.go
@@ -12,3 +12,13 @@ type Phase struct {
 	ReleaseRetentionPolicy             *core.RetentionPeriod `json:"ReleaseRetentionPolicy"`
 	TentacleRetentionPolicy            *core.RetentionPeriod `json:"TentacleRetentionPolicy"`
 }
+
+func NewPhase(name string) *Phase {
+	return &Phase{
+		AutomaticDeploymentTargets: []string{},
+		Name:                       name,
+		OptionalDeploymentTargets:  []string{},
+		ReleaseRetentionPolicy:     core.NewRetentionPeriod(30, "Days", false),
+		TentacleRetentionPolicy:    core.NewRetentionPeriod(30, "Days", false),
+	}
+}
