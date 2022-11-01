c965a6e2d703d21b97daa4e6eeea800e52bdd5ce
John Bristowe <john.bristowe@octopus.com>
2022-11-01 16:14:47 +1000

    chore: added test

--- pkg/credentials/resource_test.go
+++ pkg/credentials/resource_test.go
@@ -80,3 +80,37 @@ func TestResourceWithAnonymousAsJSON(t *testing.T) {
 
 	jsonassert.New(t).Assertf(expectedJSON, string(resourceAsJSON))
 }
+
+func TestResourceWithReferenceAsJSON(t *testing.T) {
+	description := internal.GetRandomName()
+	id := internal.GetRandomName()
+	name := internal.GetRandomName()
+	selfLink := internal.GetRandomName()
+
+	reference := credentials.NewReference(id)
+
+	referenceAsJSON, err := json.Marshal(reference)
+	require.NoError(t, err)
+	require.NotNil(t, referenceAsJSON)
+
+	resource := credentials.NewResource(name, reference)
+	resource.Description = description
+	resource.ID = id
+	resource.Links["Self"] = selfLink
+
+	expectedJSON := fmt.Sprintf(`{
+		"Description": "%s",
+		"Details": %s,
+		"Id": "%s",
+		"Name": "%s",
+		"Links": {
+			"Self": "%s"
+		}
+	}`, description, referenceAsJSON, id, name, selfLink)
+
+	resourceAsJSON, err := json.Marshal(resource)
+	require.NoError(t, err)
+	require.NotNil(t, resourceAsJSON)
+
+	jsonassert.New(t).Assertf(expectedJSON, string(resourceAsJSON))
+}
