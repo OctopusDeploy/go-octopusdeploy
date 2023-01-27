5d2e50bc8ec66e3ed89569360accd60a4ff88207
Ben Pearce <ben.pearce@octopus.com>
2023-01-27 11:31:30 +1000

    remove unused function

--- pkg/users/api_key_service_test.go
+++ pkg/users/api_key_service_test.go
@@ -47,32 +47,6 @@ func TestAPIKeyServiceGetWithEmptyID(t *testing.T) {
 	assert.Nil(t, resource)
 }
 
-func createServiceAccountUser(t *testing.T) *User {
-	service := NewUserService(nil,
-		constants.TestURIUsers,
-		constants.TestURIAPIKeys,
-		constants.TestURIAuthenticateOctopusID,
-		constants.TestURICurrentUser,
-		constants.TestURIExternalUserSearch,
-		constants.TestURIRegister,
-		constants.TestURISignIn,
-		constants.TestURISignOut,
-		constants.TestURIUserAuthentication,
-		constants.TestURIUserIdentityMetadata,
-	)
-	services.NewServiceTests(t, service, constants.TestURIUsers, constants.ServiceUserService)
-
-	user := NewUser(internal.GetRandomName(), internal.GetRandomName())
-	user.IsService = true
-	assert.NotNil(t, user)
-
-	resource, err := service.Add(user)
-	assert.NoError(t, err)
-	assert.NotNil(t, resource)
-
-	return resource
-}
-
 func createAPIKeyService(t *testing.T) *ApiKeyService {
 	service := NewAPIKeyService(nil, constants.TestURIAPIKeys)
 	services.NewServiceTests(t, service, constants.TestURIAPIKeys, constants.ServiceAPIKeyService)
