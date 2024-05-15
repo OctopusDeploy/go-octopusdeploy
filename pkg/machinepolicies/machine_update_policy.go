0b9dc7d9a9567d55c6156b7f70116976f8e9fbca
Alastair Pitts <APErebus@users.noreply.github.com>
2024-05-15 11:44:14 +1000

    Add KubernetesAgentUpdateBehavior to MachinePolicy.MachineUpdatePolicy (#250)
    * Add KubernetesAgentUpdateBehavior to MachinePolicy.MachineUpdatePolicy
    
    * Change to use Artifactory Server docker container
    
    * Use Artifactory container

--- pkg/machinepolicies/machine_update_policy.go
+++ pkg/machinepolicies/machine_update_policy.go
@@ -1,14 +1,16 @@
 package machinepolicies
 
 type MachineUpdatePolicy struct {
-	CalamariUpdateBehavior  string `json:"CalamariUpdateBehavior" validate:"required,oneof=UpdateAlways UpdateOnDeployment UpdateOnNewMachine"`
-	TentacleUpdateAccountID string `json:"TentacleUpdateAccountId,omitempty"`
-	TentacleUpdateBehavior  string `json:"TentacleUpdateBehavior" validate:"required,oneof=NeverUpdate Update"`
+	CalamariUpdateBehavior        string `json:"CalamariUpdateBehavior" validate:"required,oneof=UpdateAlways UpdateOnDeployment UpdateOnNewMachine"`
+	TentacleUpdateAccountID       string `json:"TentacleUpdateAccountId,omitempty"`
+	TentacleUpdateBehavior        string `json:"TentacleUpdateBehavior" validate:"required,oneof=NeverUpdate Update"`
+	KubernetesAgentUpdateBehavior string `json:"KubernetesAgentUpdateBehavior" validate:"required,oneof=NeverUpdate Update"`
 }
 
 func NewMachineUpdatePolicy() *MachineUpdatePolicy {
 	return &MachineUpdatePolicy{
-		CalamariUpdateBehavior: "UpdateOnDeployment",
-		TentacleUpdateBehavior: "NeverUpdate",
+		CalamariUpdateBehavior:        "UpdateOnDeployment",
+		TentacleUpdateBehavior:        "NeverUpdate",
+		KubernetesAgentUpdateBehavior: "Update",
 	}
 }
