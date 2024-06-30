26deac2fe738c4f7d69885007e28c95be67039ee
Orion Edwards <orion.edwards@octopus.com>
2024-07-01 06:51:09 +1200

    feat: Add support for delta file uploads using go-octodiff

--- pkg/packages/package_service.go
+++ pkg/packages/package_service.go
@@ -2,6 +2,7 @@ package packages
 
 import (
 	"encoding/json"
+	"errors"
 	"github.com/OctopusDeploy/go-octopusdeploy/v2/internal"
 	"github.com/OctopusDeploy/go-octopusdeploy/v2/pkg/constants"
 	"github.com/OctopusDeploy/go-octopusdeploy/v2/pkg/core"
@@ -13,6 +14,10 @@ import (
 	"github.com/dghubble/sling"
 	"io"
 	"net/http"
+	"os"
+	"path/filepath"
+	"regexp"
+	"strings"
 )
 
 type PackageService struct {
@@ -41,14 +46,24 @@ func NewPackageService(sling *sling.Sling, uriTemplate string, deltaSignaturePat
 // GetAll returns all packages. If none can be found or an error occurs, it
 // returns an empty collection.
 func (s *PackageService) GetAll() ([]*Package, error) {
-	items := []*Package{}
-	path, err := services.GetAllPath(s)
-	if err != nil {
-		return items, err
+	path := s.GetBasePath() // to get all packages we just hit /api/Spaces-NN/packages
+
+	var packages []*Package
+
+	loadNextPage := true
+	for loadNextPage {
+		resp, err := api.ApiGet(s.GetClient(), new(resources.Resources[*Package]), path)
+
+		if err != nil {
+			return packages, err
+		}
+
+		r := resp.(*resources.Resources[*Package])
+		packages = append(packages, r.Items...)
+		path, loadNextPage = services.LoadNextPage(r.PagedResults)
 	}
 
-	_, err = api.ApiGet(s.GetClient(), &items, path)
-	return items, err
+	return packages, nil
 }
 
 // GetByID returns the package that matches the input ID. If one cannot be
@@ -99,43 +114,93 @@ func (s *PackageService) Update(octopusPackage *Package) (*Package, error) {
 // - fileName: The string which we tell the server to use for the file name (may not necessarily be an actual filename on disk)
 // - reader: io.Reader which provides the binary file data to upload
 // - overwriteMode: Instructs the server what to do in the case that the package already exists.
+//
+// Return values:
+// - PackageUploadResponse: The server's response to the upload request
+// - bool: True if the server created a new file, false if it ignored an existing file
+// - error: Any error that occurred during the upload process
 func Upload(client newclient.Client, spaceID string, fileName string, reader io.Reader, overwriteMode OverwriteMode) (*PackageUploadResponse, bool, error) {
+	// directly uploading a file only requires a forward-moving `io.Reader`.
+	//
+	// UploadV2 requires `io.ReadSeeker` because delta upload needs it.
+	// If we pass useDeltaCompression: false to UploadV2 then it promises not to
+	// call Seek, so we can preserve our existing `reader` contract here by faking it out
+
+	v2Response, err := UploadV2(client, spaceID, fileName, &fakeReadSeeker{reader: reader}, overwriteMode, false)
+	if err != nil {
+		return nil, false, err
+	}
+	return &v2Response.PackageUploadResponse, v2Response.CreatedNewFile, nil
+}
+
+func UploadV2(client newclient.Client, spaceID string, fileName string, reader io.ReadSeeker, overwriteMode OverwriteMode, useDeltaCompression bool) (*PackageUploadResponseV2, error) {
 	if client == nil {
-		return nil, false, internal.CreateRequiredParameterIsEmptyOrNilError("client")
+		return nil, internal.CreateRequiredParameterIsEmptyOrNilError("client")
 	}
 	if spaceID == "" {
-		return nil, false, internal.CreateRequiredParameterIsEmptyOrNilError("spaceID")
+		return nil, internal.CreateRequiredParameterIsEmptyOrNilError("spaceID")
 	}
 	if fileName == "" {
-		return nil, false, internal.CreateRequiredParameterIsEmptyOrNilError("fileName")
+		return nil, internal.CreateRequiredParameterIsEmptyOrNilError("fileName")
 	}
 	if reader == nil {
-		return nil, false, internal.CreateRequiredParameterIsEmptyOrNilError("reader")
+		return nil, internal.CreateRequiredParameterIsEmptyOrNilError("reader")
 	}
+
+	if useDeltaCompression {
+		fileLength, err := reader.Seek(0, io.SeekEnd)
+		if err != nil {
+			return nil, err
+		}
+		_, err = reader.Seek(0, io.SeekStart)
+		if err != nil {
+			return nil, err
+		}
+		return uploadDelta(client, spaceID, fileName, reader, fileLength, overwriteMode)
+	} else {
+		// push complete package to server
+		params := map[string]any{
+			"spaceId": spaceID,
+		}
+		if overwriteMode != "" {
+			params["overwriteMode"] = overwriteMode
+		}
+
+		expandedUri, err := client.URITemplateCache().Expand(uritemplates.PackageUpload, params)
+		if err != nil {
+			return nil, err
+		}
+
+		stdResponse, createdNewFile, err := httpUploadPackageFile(client, expandedUri, fileName, reader)
+		if err != nil {
+			return nil, err
+		}
+
+		return &PackageUploadResponseV2{
+			CreatedNewFile:        createdNewFile,
+			UploadMethod:          UploadMethodStandard,
+			PackageUploadResponse: *stdResponse,
+			UploadInfo:            nil,
+		}, nil
+
+	}
+}
+
+// httpUploadFile creates a multipart POST to upload a file and sends it to the server for either a full package upload
+// or a delta upload. Both have the same response type so we deserialize that too
+func httpUploadPackageFile(client newclient.Client, url string, fileName string, reader io.Reader) (response *PackageUploadResponse, createdNewFile bool, err error) {
 
 	multipartWriter := NewMultipartFileStreamingReader(fileName, reader)
 
-	params := map[string]any{
-		"spaceId": spaceID,
-	}
-	if overwriteMode != "" {
-		params["overwriteMode"] = overwriteMode
-	}
-
-	expandedUri, err := client.URITemplateCache().Expand(uritemplates.PackageUpload, params)
+	req, err := http.NewRequest(http.MethodPost, url, multipartWriter)
 	if err != nil {
-		return nil, false, err
-	}
-
-	req, err := http.NewRequest(http.MethodPost, expandedUri, multipartWriter)
-	if err != nil {
-		return nil, false, err
+		return
 	}
 	req.Header.Add("Content-Type", multipartWriter.FormDataContentType())
 
 	resp, err := client.HttpSession().DoRawRequest(req)
 	if err != nil {
-		return nil, false, err
+		return
 	}
 	defer newclient.CloseResponse(resp)
 
@@ -144,18 +209,20 @@ func Upload(client newclient.Client, spaceID string, fileName string, reader io.
 		outputResponseBody := new(PackageUploadResponse)
 		err = bodyDecoder.Decode(outputResponseBody)
 		if err != nil {
-			return nil, false, err
+			return
 		}
 		// the server returns 201 if it created a new file, 200 if it ignored an existing file
-		createdNewFile := resp.StatusCode == http.StatusCreated
-		return outputResponseBody, createdNewFile, nil
+		createdNewFile = resp.StatusCode == http.StatusCreated
+		response = outputResponseBody
+		return
 	} else {
 		outputResponseError := new(core.APIError)
 		err = bodyDecoder.Decode(outputResponseError)
 		if err != nil {
-			return nil, false, err
+			return
 		}
-		return nil, false, outputResponseError
+		err = outputResponseError
+		return
 	}
 }
 
@@ -177,8 +244,66 @@ func List(client newclient.Client, spaceID string, filter string, limit int) (*r
 		return nil, err
 	}
 
-	if err != nil {
-		return nil, err
-	}
 	return newclient.Get[resources.Resources[*Package]](client.HttpSession(), expandedUri)
+}
+
+// ---- internal helpers -----
+
+func withoutLastExtension(path string) string {
+	for i := len(path) - 1; i >= 0 && !os.IsPathSeparator(path[i]); i-- {
+		if path[i] == '.' {
+			return path[:i]
+		}
+	}
+	return path
+}
+
+// conventional wisdom in go is that the file extension is always the thing after the last .
+// but this mishandles .tar.gz files, so we need a special case to handle them (and .tar.bz2 etc)
+func removeExtension(fileName string) string {
+	candidate := withoutLastExtension(fileName)
+	if len(candidate) > 3 && strings.EqualFold(filepath.Ext(candidate), ".tar") {
+		return withoutLastExtension(candidate)
+	}
+	return candidate
+}
+
+// ParsePackageIDAndVersion ported from OctopusServer's PackageIdentity class
+// See PackageIdentityParser in the C# Octopus Client SDK
+// Note: Unlike in C#, fileName here includes the file extension
+func ParsePackageIDAndVersion(fileName string) (packageID string, version string, err error) {
+	pattern := regexp.MustCompile(
+		"^" + // start of line
+			"(?P<packageId>(\\w+([_.-]\\w+)*?))" + // Package ID
+			"\\." + // version separator
+			"(?P<semanticVersion>(\\d+(\\.\\d+){0,3}" + // Major Minor Patch
+			"(-[0-9A-Za-z-]+(\\.[0-9A-Za-z-]+)*)?)" + // Pre-release identifiers
+			"(\\+[0-9A-Za-z-]+(\\.[0-9A-Za-z-]+)*)?)" + // Build Metadata
+			"$") // EOL
+
+	match := pattern.FindStringSubmatch(removeExtension(fileName))
+	if match == nil {
+		err = errors.New("could not determine the package ID and/or version based on the supplied filename")
+		return
+	}
+	for i, name := range pattern.SubexpNames() {
+		if name == "packageId" {
+			packageID = match[i]
+		} else if name == "semanticVersion" {
+			version = match[i]
+		}
+	}
+	return
+}
+
+type fakeReadSeeker struct {
+	reader io.Reader
+}
+
+func (f *fakeReadSeeker) Read(p []byte) (n int, err error) {
+	return f.reader.Read(p)
+}
+
+func (f *fakeReadSeeker) Seek(offset int64, whence int) (int64, error) {
+	return 0, errors.New("seek is not supported for fakeReadSeeker")
 }
--- uritemplates/links.go
+++ uritemplates/links.go
@@ -5,32 +5,43 @@ package uritemplates
 // NOTE: If the server *were* to ever remove an api link, we'd need to do some sort of version detection to compensate.
 
 const (
-	BuildInformation                    = "/api/{spaceId}/build-information{/id}{?packageId,filter,latest,skip,take,overwriteMode}"
-	BuildInformationBulk                = "/api/{spaceId}/build-information/bulk{?ids}"
-	CreateReleaseCommandV1              = "/api/{spaceId}/releases/create/v1"                                                                                                             // POST
-	CreateDeploymentTenantedCommandV1   = "/api/{spaceId}/deployments/create/tenanted/v1"                                                                                                 // POST
-	CreateDeploymentUntenantedCommandV1 = "/api/{spaceId}/deployments/create/untenanted/v1"                                                                                               // POST
-	CreateRunRunbookCommand             = "/api/{spaceId}/runbook-runs/create/v1"                                                                                                         // POST
-	DeploymentProcesses                 = "/api/{spaceId}/deploymentprocesses{/id}{?skip,take,ids}"                                                                                       // GET
-	FeedSearchPackageVersions           = "/api/{spaceId}/feeds/{feedId}/packages/versions{?packageId,take,skip,includePreRelease,versionRange,preReleaseTag,filter,includeReleaseNotes}" // GET
+	BuildInformation     = "/api/{spaceId}/build-information{/id}{?packageId,filter,latest,skip,take,overwriteMode}"
+	BuildInformationBulk = "/api/{spaceId}/build-information/bulk{?ids}"
+
+	CreateReleaseCommandV1 = "/api/{spaceId}/releases/create/v1" // POST
+
+	CreateDeploymentTenantedCommandV1   = "/api/{spaceId}/deployments/create/tenanted/v1"   // POST
+	CreateDeploymentUntenantedCommandV1 = "/api/{spaceId}/deployments/create/untenanted/v1" // POST
+
+	CreateRunRunbookCommand = "/api/{spaceId}/runbook-runs/create/v1" // POST
+
+	DeploymentProcesses = "/api/{spaceId}/deploymentprocesses{/id}{?skip,take,ids}" // GET
+
+	FeedSearchPackageVersions = "/api/{spaceId}/feeds/{feedId}/packages/versions{?packageId,take,skip,includePreRelease,versionRange,preReleaseTag,filter,includeReleaseNotes}" // GET
+
+	PackageDeltaSignature = "/api/{spaceId}/packages/{packageId}/{version}/delta-signature"                      // GET
+	PackageDeltaUpload    = "/api/{spaceId}/packages/{packageId}/{baseVersion}/delta{?replace,overwriteMode}"    // POST
 	Packages                            = "/api/{spaceId}/packages{/id}{?nuGetPackageId,filter,latest,skip,take,includeNotes}"                                                            // GET
 	LibraryVariableSets                 = "/api/{spaceId}/libraryvariablesets{/id}{?skip,contentType,take,ids,partialName}"
-	PackageUpload                       = "/api/{spaceId}/packages/raw{?replace,overwriteMode}"                                                               // POST multipart form
-	ReleaseDeploymentPreview            = "/api/{spaceId}/releases/{releaseId}/deployments/preview/{environmentId}{?includeDisabledSteps}"                    // GET
+	PackageUpload                       = "/api/{spaceId}/packages/raw{?replace,overwriteMode}"                                                                                           // POST multipart form
+
+	ReleaseDeploymentPreview            = "/api/{spaceId}/releases/{releaseId}/deployments/preview/{environmentId}{?includeDisabledSteps}"                                                // GET
 	ReleaseDeploymentPreviews           = "/api/{spaceId}/releases/{releaseId}/deployments/previews"                                                          // POST multipart form
-	Releases                            = "/api/{spaceId}/releases{/id}{?skip,ignoreChannelRules,take,ids}"                                                   // GET
-	ReleasesByProject                   = "/api/{spaceId}/projects/{projectId}/releases{/version}{?skip,take,searchByVersion}"                                // GET
-	ReleasesByProjectAndChannel         = "/api/{spaceId}/projects/{projectId}/channels/{channelId}/releases{?skip,take,searchByVersion}"                     // GET
-	Runbooks                            = "/api/{spaceId}/runbooks{/id}{?skip,take,ids,partialName,clone,projectIds}"                                         // GET
-	RunbooksByProject                   = "/api/{spaceId}/projects/{projectId}/runbooks{?skip,take,partialName}"                                              // GET
-	RunbookEnvironments                 = "/api/{spaceId}/projects/{projectId}/runbooks/{runbookId}/environments"                                             // GET
-	RunbookProcess                      = "/api/{spaceId}/projects/{projectId}/runbookProcesses/{id}"                                                         // GET
-	RunbookRunPreview                   = "/api/{spaceId}/projects/{projectId}/runbooks/{runbookId}/runbookRuns/preview/{environment}{?includeDisabledSteps}" // GET
-	RunbookSnapshotsByRunbook           = "/api/{spaceId}/projects/{projectId}/runbooks/{runbookId}/runbookSnapshots{/name}{?skip,take,searchByName}"         // GET
-	RunbookSnapshotsByProject           = "/api/{spaceId}/projects/{projectId}/runbookSnapshots{/name}{?skip,take,searchByName}"                              // GET
-	RunbookSnapshotRunPreview           = "/api/{spaceId}/runbookSnapshots/{snapshotId}/runbookRuns/preview/{environmentId}{?includeDisabledSteps}"           // GET
-	RunbookRunTenantPreview             = "/api/{spaceId}/projects/{projectId}/runbooks/{runbookId}/runbookRuns/previews"                                     // POST
-	Variables                           = "/api/{spaceId}/variables{/id}{?ids}"                                                                               // GET
+	Releases                            = "/api/{spaceId}/releases{/id}{?skip,ignoreChannelRules,take,ids}"                                                                               // GET
+	ReleasesByProject                   = "/api/{spaceId}/projects/{projectId}/releases{/version}{?skip,take,searchByVersion}"                                                            // GET
+	ReleasesByProjectAndChannel         = "/api/{spaceId}/projects/{projectId}/channels/{channelId}/releases{?skip,take,searchByVersion}"                                                 // GET
+
+	Runbooks                            = "/api/{spaceId}/runbooks{/id}{?skip,take,ids,partialName,clone,projectIds}"                                                                     // GET
+	RunbooksByProject                   = "/api/{spaceId}/projects/{projectId}/runbooks{?skip,take,partialName}"                                                                          // GET
+	RunbookEnvironments                 = "/api/{spaceId}/projects/{projectId}/runbooks/{runbookId}/environments"                                                                         // GET
+	RunbookProcess                      = "/api/{spaceId}/projects/{projectId}/runbookProcesses/{id}"                                                                                     // GET
+	RunbookRunPreview                   = "/api/{spaceId}/projects/{projectId}/runbooks/{runbookId}/runbookRuns/preview/{environment}{?includeDisabledSteps}"                             // GET
+	RunbookSnapshotsByRunbook           = "/api/{spaceId}/projects/{projectId}/runbooks/{runbookId}/runbookSnapshots{/name}{?skip,take,searchByName}"                                     // GET
+	RunbookSnapshotsByProject           = "/api/{spaceId}/projects/{projectId}/runbookSnapshots{/name}{?skip,take,searchByName}"                                                          // GET
+	RunbookSnapshotRunPreview           = "/api/{spaceId}/runbookSnapshots/{snapshotId}/runbookRuns/preview/{environmentId}{?includeDisabledSteps}"                                       // GET
+	RunbookRunTenantPreview             = "/api/{spaceId}/projects/{projectId}/runbooks/{runbookId}/runbookRuns/previews"                                                                 // POST
+
+	Variables                           = "/api/{spaceId}/variables{/id}{?ids}"                                                                                                           // GET
 	ProjectVariablesByGitRef            = "/api/{spaceId}/projects/{projectId}/{gitRef}/variables"
 	ProjectBranchesV2                   = "/api/{spaceId}/projects/{projectId}/git/branches/v2"
 	ProjectBranches                     = "/api/{spaceId}/projects/{projectId}/git/branches"
