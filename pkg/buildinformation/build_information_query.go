87bd1524aca1b59ea5c2df29a41c85913092a15c
hnrkndrssn <henrik@henrikandersson.org>
2024-07-10 15:39:52 +1000

    fix: latest flag on the query should be a boolean

--- pkg/buildinformation/build_information_query.go
+++ pkg/buildinformation/build_information_query.go
@@ -2,7 +2,7 @@ package buildinformation
 
 type BuildInformationQuery struct {
 	Filter           string `uri:"filter,omitempty" url:"filter,omitempty"`
-	Latest           string `uri:"latest,omitempty" url:"latest,omitempty"`
+	Latest           bool   `uri:"latest,omitempty" url:"latest,omitempty"`
 	OverwriteMode    string `uri:"overwriteMode,omitempty" url:"overwriteMode,omitempty"`
 	PackageID        string `uri:"packageId,omitempty" url:"packageId,omitempty"`
 	IncludeWorkItems bool   `uri:"includeWorkItems,omitempty" url:"includeWorkItems,omitempty"`
