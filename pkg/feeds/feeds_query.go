dd2d118495217bbbc0be80b7e0866e83b07153ba
isaac <isaac.calligeros@octopus.com>
2024-08-13 16:00:38 +0930

    Add feed name to search functionality

--- pkg/feeds/feeds_query.go
+++ pkg/feeds/feeds_query.go
@@ -1,6 +1,7 @@
 package feeds
 
 type FeedsQuery struct {
+	Name        string   `uri:"name,omitempty" url:"name,omitempty"`
 	FeedType    string   `uri:"feedType,omitempty" url:"feedType,omitempty"`
 	IDs         []string `uri:"ids,omitempty" url:"ids,omitempty"`
 	PartialName string   `uri:"partialName,omitempty" url:"partialName,omitempty"`
