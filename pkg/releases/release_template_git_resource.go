c05864a56abbc8b3c26ed5ced17b0f255291684a
Alastair Pitts <alastair.pitts@octopus.com>
2024-09-09 13:20:53 +1000

    Add GitResources to DeploymentProcessTemplate

---
+++ pkg/releases/release_template_git_resource.go
@@ -0,0 +1,17 @@
+﻿package releases
+
+type ReleaseTemplateGitResource struct {
+	ActionName                     string                      `json:"ActionName,omitempty"`
+	RepositoryUri                  string                      `json:"RepositoryUri,omitempty"`
+	DefaultBranch                  string                      `json:"DefaultBranch,omitempty"`
+	IsResolvable                   bool                        `json:"IsResolvable"`
+	Name                           string                      `json:"Name,omitempty"`
+	FilePathFilters                []string                    `json:"FilePathFilters,omitempty"`
+	GitCredentialId                string                      `json:"NuGetPackageId,omitempty"`
+	GitResourceSelectedLastRelease ReleaseTemplateGitReference `json:"GitResourceSelectedLastRelease,omitempty"`
+}
+
+type ReleaseTemplateGitReference struct {
+	GitRef    string `json:"GitRef,omitempty"`
+	GitCommit string `json:"GitCommit,omitempty"`
+}
