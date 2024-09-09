8e9ae276a98fedc3585b4f03741436c1297f77a2
Alastair Pitts <alastair.pitts@octopus.com>
2024-09-09 13:22:29 +1000

    Use existing resource

--- pkg/releases/release_template_git_resource.go
+++ pkg/releases/release_template_git_resource.go
@@ -1,17 +1,14 @@
 ﻿package releases
 
-type ReleaseTemplateGitResource struct {
-	ActionName                     string                      `json:"ActionName,omitempty"`
-	RepositoryUri                  string                      `json:"RepositoryUri,omitempty"`
-	DefaultBranch                  string                      `json:"DefaultBranch,omitempty"`
-	IsResolvable                   bool                        `json:"IsResolvable"`
-	Name                           string                      `json:"Name,omitempty"`
-	FilePathFilters                []string                    `json:"FilePathFilters,omitempty"`
-	GitCredentialId                string                      `json:"NuGetPackageId,omitempty"`
-	GitResourceSelectedLastRelease ReleaseTemplateGitReference `json:"GitResourceSelectedLastRelease,omitempty"`
-}
+import "github.com/OctopusDeploy/go-octopusdeploy/v2/pkg/actions"
 
-type ReleaseTemplateGitReference struct {
-	GitRef    string `json:"GitRef,omitempty"`
-	GitCommit string `json:"GitCommit,omitempty"`
+type ReleaseTemplateGitResource struct {
+	ActionName                     string                          `json:"ActionName,omitempty"`
+	RepositoryUri                  string                          `json:"RepositoryUri,omitempty"`
+	DefaultBranch                  string                          `json:"DefaultBranch,omitempty"`
+	IsResolvable                   bool                            `json:"IsResolvable"`
+	Name                           string                          `json:"Name,omitempty"`
+	FilePathFilters                []string                        `json:"FilePathFilters,omitempty"`
+	GitCredentialId                string                          `json:"NuGetPackageId,omitempty"`
+	GitResourceSelectedLastRelease actions.VersionControlReference `json:"GitResourceSelectedLastRelease,omitempty"`
 }
