d53c52f2e057a410a4e605edfc867372cbc5dfab
Ben Pearce <ben.pearce@octopus.com>
2024-11-28 15:46:13 +1000

    fix: added Partial Name and Status to deployment freeze query (#290)

--- pkg/deploymentfreezes/deploymentfreeze_query.go
+++ pkg/deploymentfreezes/deploymentfreeze_query.go
@@ -2,6 +2,8 @@ package deploymentfreezes
 
 type DeploymentFreezeQuery struct {
 	IncludeComplete bool     `uri:"includeComplete,omitempty" url:"includeComplete,omitempty"`
+	PartialName     string   `uri:"partialName,omitempty" url:"partialName,omitempty"`
+	Status          string   `uri:"status,omitempty" url:"status,omitempty"`
 	ProjectIds      []string `uri:"projectIds,omitempty" url:"projectIds,omitempty"`
 	EnvironmentIds  []string `uri:"environmentIds,omitempty" url:"environmentIds,omitempty"`
 	IDs             []string `uri:"ids,omitempty" url:"ids,omitempty"`
