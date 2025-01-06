cf8b041d6c5c94b3a82a968e2e38b703e4caf89a
Matthew Casperson <matthewcasperson@gmail.com>
2025-01-06 10:08:54 +1000

    Added support for github credentials

--- pkg/projects/persistence_settings.go
+++ pkg/projects/persistence_settings.go
@@ -8,5 +8,4 @@ type PersistenceSettings interface {
 // persistenceSettings represents persistence settings associated with a project.
 type persistenceSettings struct {
 	SettingsType PersistenceSettingsType `json:"Type"`
-	SettingsId   *string                 `json:"Id"`
 }
