dbf2b5e66f6cf303a8d13dd367683f38261c0bd0
Huy Nguyen <huy.nguyen@octopus.com>
2025-05-14 11:38:30 +1000

    Add method to get space and server root

--- pkg/client/root_service.go
+++ pkg/client/root_service.go
@@ -2,6 +2,7 @@ package client
 
 import (
 	"github.com/OctopusDeploy/go-octopusdeploy/v2/pkg/constants"
+	"github.com/OctopusDeploy/go-octopusdeploy/v2/pkg/newclient"
 	"github.com/OctopusDeploy/go-octopusdeploy/v2/pkg/services"
 	"github.com/OctopusDeploy/go-octopusdeploy/v2/pkg/services/api"
 	"github.com/dghubble/sling"
@@ -32,3 +33,24 @@ func (s *RootService) Get() (*RootResource, error) {
 }
 
 var _ services.IService = &RootService{}
+
+const (
+	template = "/api/{spaceId}"
+)
+
+func GetSpaceRoot(client newclient.Client, spaceID *string) (*RootResource, error) {
+	values := map[string]any{}
+	if spaceID != nil {
+		values["spaceId"] = *spaceID
+	}
+	path, err := client.URITemplateCache().Expand(template, values)
+
+	if err != nil {
+		return nil, err
+	}
+	return newclient.Get[RootResource](client.HttpSession(), path)
+}
+
+func GetServerRoot(client newclient.Client) (*RootResource, error) {
+	return GetSpaceRoot(client, nil)
+}
