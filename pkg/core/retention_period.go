8781c665486f27f1957d64f76aba3683aa643cee
Susan P <82346985+susanpann@users.noreply.github.com>
2025-08-15 13:19:52 +1000

    feat: Centralised Retention - Add support for setting space default on lifecycle retention policies (#353)
    * update retentionperiod
    
    * method and tests
    
    * keep old function

--- pkg/core/retention_period.go
+++ pkg/core/retention_period.go
@@ -1,15 +1,88 @@
 package core
 
+import (
+	"encoding/json"
+)
+
 type RetentionPeriod struct {
 	QuantityToKeep    int32  `json:"QuantityToKeep"`
 	ShouldKeepForever bool   `json:"ShouldKeepForever"`
 	Unit              string `json:"Unit"`
+	Strategy          string `json:"Strategy,omitempty"`
 }
 
+const (
+	RetentionStrategyDefault string = "Default"
+	RetentionStrategyForever string = "Forever"
+	RetentionStrategyCount   string = "Count"
+)
+
+const (
+	RetentionUnitDays  string = "Days"
+	RetentionUnitItems string = "Items"
+)
+
 func NewRetentionPeriod(quantityToKeep int32, unit string, shouldKeepForever bool) *RetentionPeriod {
+	if shouldKeepForever {
+		return KeepForeverRetentionPeriod()
+	} else {
+		return CountBasedRetentionPeriod(quantityToKeep, unit)
+	}
+}
+
+func CountBasedRetentionPeriod(quantityToKeep int32, unit string) *RetentionPeriod {
 	return &RetentionPeriod{
 		QuantityToKeep:    quantityToKeep,
-		ShouldKeepForever: shouldKeepForever,
 		Unit:              unit,
+		ShouldKeepForever: false,
+		Strategy:          RetentionStrategyCount,
 	}
+}
+
+func KeepForeverRetentionPeriod() *RetentionPeriod {
+	return &RetentionPeriod{
+		QuantityToKeep:    0,
+		Unit:              RetentionUnitItems,
+		ShouldKeepForever: true,
+		Strategy:          RetentionStrategyForever,
+	}
+}
+
+func SpaceDefaultRetentionPeriod() *RetentionPeriod {
+	return &RetentionPeriod{
+		QuantityToKeep:    0,
+		Unit:              RetentionUnitItems,
+		ShouldKeepForever: true,
+		Strategy:          RetentionStrategyDefault,
+	}
+}
+
+// UnmarshalJSON sets a retention period to its representation in JSON.
+func (r *RetentionPeriod) UnmarshalJSON(data []byte) error {
+	var fields struct {
+		QuantityToKeep    int32  `json:"QuantityToKeep"`
+		ShouldKeepForever bool   `json:"ShouldKeepForever"`
+		Unit              string `json:"Unit"`
+		Strategy          string `json:"Strategy,omitempty"`
+	}
+
+	if err := json.Unmarshal(data, &fields); err != nil {
+		return err
+	}
+
+	r.QuantityToKeep = fields.QuantityToKeep
+	r.ShouldKeepForever = fields.ShouldKeepForever
+	r.Unit = fields.Unit
+
+	if fields.Strategy == "" {
+		if r.ShouldKeepForever == true {
+			r.Strategy = RetentionStrategyForever
+		} else {
+			r.Strategy = RetentionStrategyCount
+		}
+	} else {
+		r.Strategy = fields.Strategy
+	}
+
+	return nil
 }
