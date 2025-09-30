0b3dcc3e8d21078206f4f6c754c7b8883c48620f
Grace Rehn <grace.rehn@octopus.com>
2025-09-30 14:46:56 +1000

    feat: Add TagSetScope and TagSetType

--- pkg/tagsets/tag_set.go
+++ pkg/tagsets/tag_set.go
@@ -2,14 +2,29 @@ package tagsets
 
 import "github.com/OctopusDeploy/go-octopusdeploy/v2/pkg/resources"
 
+type TagSetScope string
+
+const (
+	TagSetScopeTenant      TagSetScope = "Tenant"
+	TagSetScopeEnvironment TagSetScope = "Environment"
+)
+
+type TagSetType string
+
+const (
+	TagSetTypeSingleSelect TagSetType = "SingleSelect"
+	TagSetTypeMultiSelect  TagSetType = "MultiSelect"
+	TagSetTypeFreeText     TagSetType = "FreeText"
+)
+
 type TagSet struct {
-	Description string   `json:"Description"`
-	Name        string   `json:"Name"`
-	Scopes      []string `json:"Scopes,omitempty"`
-	SortOrder   int32    `json:"SortOrder,omitempty"`
-	SpaceID     string   `json:"SpaceId,omitempty"`
-	Tags        []*Tag   `json:"Tags,omitempty"`
-	Type        string   `json:"Type,omitempty"`
+	Description string        `json:"Description"`
+	Name        string        `json:"Name"`
+	Scopes      []TagSetScope `json:"Scopes,omitempty"`
+	SortOrder   int32         `json:"SortOrder,omitempty"`
+	SpaceID     string        `json:"SpaceId,omitempty"`
+	Tags        []*Tag        `json:"Tags,omitempty"`
+	Type        TagSetType    `json:"Type,omitempty"`
 
 	resources.Resource
 }
@@ -18,8 +33,8 @@ type TagSet struct {
 func NewTagSet(name string) *TagSet {
 	return &TagSet{
 		Name:     name,
-		Scopes:   []string{"Tenant"},
-		Type:     "MultiSelect",
+		Scopes:   []TagSetScope{TagSetScopeTenant},
+		Type:     TagSetTypeMultiSelect,
 		Resource: *resources.NewResource(),
 	}
 }
