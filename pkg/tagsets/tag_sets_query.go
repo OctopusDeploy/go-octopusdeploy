7be5b70731e69aba633c636f7af7bd1c70ff3e12
Grace Rehn <grace.rehn@octopus.com>
2025-10-02 12:54:10 +1000

    fix: Tag Set query missing scopes url struct tag

--- pkg/tagsets/tag_sets_query.go
+++ pkg/tagsets/tag_sets_query.go
@@ -3,7 +3,7 @@ package tagsets
 type TagSetsQuery struct {
 	IDs         []string      `uri:"ids,omitempty" url:"ids,omitempty"`
 	PartialName string        `uri:"partialName,omitempty" url:"partialName,omitempty"`
-	Scopes      []TagSetScope `uri:"scopes,omitempty"`
+	Scopes      []TagSetScope `uri:"scopes,omitempty" url:"scopes,omitempty"`
 	Skip        int           `uri:"skip,omitempty" url:"skip,omitempty"`
 	Take        int           `uri:"take,omitempty" url:"take,omitempty"`
 }
