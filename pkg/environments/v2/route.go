37fd102e8b62d6fd2c4911f8ca3a3ee5fac08cb3
Steven Cleve <107827476+stevencl840@users.noreply.github.com>
2025-11-03 17:22:43 +1100

    feat: Add Ephemeral Environment Deprovisioning (#375)

--- pkg/environments/v2/route.go
+++ pkg/environments/v2/route.go
@@ -3,3 +3,4 @@ package v2
 const Template = "/api/{spaceId}/environments/v2{/id}{?name,skip,ids,take,partialName,type}"
 const CreateEphemeralEnvironmentTemplate = "/api/{spaceId}/projects/{projectId}/environments/ephemeral"
 const DeprovisionEphemeralEnvironmentForProjectTemplate = "/api/{spaceId}/projects/{projectId}/environments/ephemeral/{id}/deprovision"
+const DeprovisionEphemeralEnvironmentTemplate = "/api/{spaceId}/environments/ephemeral/{id}/deprovision"
