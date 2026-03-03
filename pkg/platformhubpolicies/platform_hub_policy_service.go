d495e0d56bb6e0dcbcfa81a733ad56e508276802
denys.kostynchuk <denys.kostynchuk@octopus.com>
2026-03-04 12:39:29 +1300

    Add GetBySlug and Update operations to platform hub policies

--- pkg/platformhubpolicies/platform_hub_policy_service.go
+++ pkg/platformhubpolicies/platform_hub_policy_service.go
@@ -5,7 +5,7 @@ import (
 	"github.com/OctopusDeploy/go-octopusdeploy/v2/pkg/newclient"
 )
 
-const template = "/api/platformhub/{gitRef}/policies"
+const template = "/api/platformhub/{gitRef}/policies{/slug}"
 
 // Add creates a new Platform Hub policy.
 func Add(client newclient.Client, policy *PlatformHubPolicy, commitMessage string) (*PlatformHubPolicy, error) {
@@ -29,6 +29,35 @@ func Add(client newclient.Client, policy *PlatformHubPolicy, commitMessage strin
 	return createdPolicy, nil
 }
 
+// GetBySlug returns the Platform Hub policy that matches given GitRef and Slug.
+func GetBySlug(client newclient.Client, gitRef string, slug string) (*PlatformHubPolicy, error) {
+	path, pathError := client.URITemplateCache().Expand(template, map[string]any{"gitRef": gitRef, "slug": slug})
+	if pathError != nil {
+		return nil, pathError
+	}
+
+	policy, err := newclient.Get[PlatformHubPolicy](client.HttpSession(), path)
+	if err != nil {
+		return nil, err
+	}
+
+	return policy, nil
+}
+
+// Update modifies an existing Platform Hub policy.
+func Update(client newclient.Client, policy *PlatformHubPolicy, commitMessage string) (*PlatformHubPolicy, error) {
+	command, path, err := buildUpdateCommand(client, policy, commitMessage)
+	if err != nil {
+		return nil, err
+	}
+
+	updatedPolicy, err := newclient.Put[PlatformHubPolicy](client.HttpSession(), path, command)
+	if err != nil {
+		return nil, err
+	}
+	return updatedPolicy, nil
+}
+
 func buildAddCommand(client newclient.Client, policy *PlatformHubPolicy, commitMessage string) (*platformHubPolicyUpsertCommand, string, error) {
 	if policy == nil {
 		return nil, "", internal.CreateRequiredParameterIsEmptyOrNilError("policy")
@@ -39,6 +68,28 @@ func buildAddCommand(client newclient.Client, policy *PlatformHubPolicy, commitM
 	}
 
 	path, pathError := client.URITemplateCache().Expand(template, map[string]any{"gitRef": policy.GitRef})
+	if pathError != nil {
+		return nil, "", pathError
+	}
+
+	command := platformHubPolicyUpsertCommand{
+		ChangeDescription: commitMessage,
+		PlatformHubPolicy: *policy,
+	}
+
+	return &command, path, nil
+}
+
+func buildUpdateCommand(client newclient.Client, policy *PlatformHubPolicy, commitMessage string) (*platformHubPolicyUpsertCommand, string, error) {
+	if policy == nil {
+		return nil, "", internal.CreateRequiredParameterIsEmptyOrNilError("policy")
+	}
+
+	if validationError := policy.Validate(); validationError != nil {
+		return nil, "", internal.CreateValidationFailureError("Update", validationError)
+	}
+
+	path, pathError := client.URITemplateCache().Expand(template, map[string]any{"gitRef": policy.GitRef, "slug": policy.Slug})
 	if pathError != nil {
 		return nil, "", pathError
 	}
--- pkg/platformhubpolicies/platform_hub_policy_service_test.go
+++ pkg/platformhubpolicies/platform_hub_policy_service_test.go
@@ -12,7 +12,25 @@ import (
 
 func TestPlatformHubPolicyService_BuildAddCommand(t *testing.T) {
 	var client = newclient.NewClient(&newclient.HttpSession{})
-	policy := newPolicyBuilder().GitRef("refs/heads/test/branch")
+
+	// Policy is nil - should fail
+	nilCommand, nilPath, commandNilError := buildAddCommand(client, nil, "commit nil command")
+	require.ErrorContains(t, commandNilError, "policy")
+	require.Nil(t, nilCommand)
+	require.Empty(t, nilPath)
+
+	// Invalid policy
+	invalidPolicy := &PlatformHubPolicy{Name: "Invalid", GitRef: ""}
+	invalidCommand, invalidPath, invalidCommandError := buildAddCommand(client, invalidPolicy, "commit invalid command")
+	require.Error(t, invalidCommandError)
+	require.Nil(t, invalidCommand)
+	require.Empty(t, invalidPath)
+
+	// Valid policy
+	policy := newPolicyBuilder().
+		GitRef("refs/heads/test/branch").
+		Description("Testing policy").
+		ViolationReason("Missing manual intervention")
 	urlEncodedGitRef := "refs%2Fheads%2Ftest%2Fbranch"
 	commitMessage := "Create new policy"
 
@@ -25,28 +43,60 @@ func TestPlatformHubPolicyService_BuildAddCommand(t *testing.T) {
 
 	require.Equal(t, fmt.Sprintf("/api/platformhub/%s/policies", urlEncodedGitRef), path)
 
-	require.NotNil(t, command)
-	require.Equal(t, policy.name, command.GetName())
-	require.Equal(t, policy.gitRef, command.GetGitRef())
-	require.Equal(t, policy.slug, command.GetSlug())
-	require.Equal(t, policy.scopeRego, command.GetScopeRego())
-	require.Equal(t, policy.conditionsRego, command.GetConditionsRego())
-	require.Equal(t, policy.violationAction, command.GetViolationAction())
-	require.Equal(t, commitMessage, command.ChangeDescription)
+	testAssertUpsertCommand(t, command, policy, commitMessage)
+	testAssertUpsertCommandMarshalJSON(t, command, policy, commitMessage)
 }
 
-func TestPlatformHubPolicyService_AddCommandMarshalJSON(t *testing.T) {
+func TestPlatformHubPolicyService_BuildUpdateCommand(t *testing.T) {
 	var client = newclient.NewClient(&newclient.HttpSession{})
-	policy := newPolicyBuilder().GitRef("main").Description("Testing policy").ViolationReason("Missing manual intervention")
-	commitMessage := "Add new policy as JSON"
+
+	// Policy is nil - should fail
+	nilCommand, nilPath, commandNilError := buildUpdateCommand(client, nil, "commit nil command")
+	require.ErrorContains(t, commandNilError, "policy")
+	require.Nil(t, nilCommand)
+	require.Empty(t, nilPath)
+
+	// Invalid policy
+	invalidPolicy := &PlatformHubPolicy{Name: "Invalid", GitRef: "main", ViolationAction: ""}
+	invalidCommand, invalidPath, invalidCommandError := buildUpdateCommand(client, invalidPolicy, "commit invalid command")
+	require.Error(t, invalidCommandError)
+	require.Nil(t, invalidCommand)
+	require.Empty(t, invalidPath)
+
+	// Valid policy
+	policy := newPolicyBuilder().Name("Valid Policy").
+		GitRef("refs/heads/main").
+		Slug("valid_policy").
+		Description("Ok").
+		ViolationReason("None")
+	urlEncodedGitRef := "refs%2Fheads%2Fmain"
+	commitMessage := "Update valid policy"
 
 	newPolicy, policyError := policy.Create()
 	require.NoError(t, policyError)
 
-	command, _, commandError := buildAddCommand(client, newPolicy, commitMessage)
+	// Act
+	command, path, commandError := buildUpdateCommand(client, newPolicy, commitMessage)
 	require.NoError(t, commandError)
 
-	// Act
+	require.Equal(t, fmt.Sprintf("/api/platformhub/%s/policies/%s", urlEncodedGitRef, policy.slug), path)
+
+	testAssertUpsertCommand(t, command, policy, commitMessage)
+	testAssertUpsertCommandMarshalJSON(t, command, policy, commitMessage)
+}
+
+func testAssertUpsertCommand(t *testing.T, command *platformHubPolicyUpsertCommand, expected *policyBuilder, expectedCommit string) {
+	require.NotNil(t, command)
+	require.Equal(t, expected.name, command.GetName())
+	require.Equal(t, expected.gitRef, command.GetGitRef())
+	require.Equal(t, expected.slug, command.GetSlug())
+	require.Equal(t, expected.scopeRego, command.GetScopeRego())
+	require.Equal(t, expected.conditionsRego, command.GetConditionsRego())
+	require.Equal(t, expected.violationAction, command.GetViolationAction())
+	require.Equal(t, expectedCommit, command.ChangeDescription)
+}
+
+func testAssertUpsertCommandMarshalJSON(t *testing.T, command *platformHubPolicyUpsertCommand, expected *policyBuilder, expectedCommit string) {
 	jsonCommand, jsonError := json.Marshal(command)
 	require.NoError(t, jsonError)
 	require.NotNil(t, jsonCommand)
@@ -61,7 +111,7 @@ func TestPlatformHubPolicyService_AddCommandMarshalJSON(t *testing.T) {
 		"ConditionsRego": "%s",
 		"ViolationReason": "%s",
 		"ViolationAction": "%s"
-	}`, commitMessage, policy.name, policy.gitRef, policy.slug, policy.description, policy.scopeRego, policy.conditionsRego, policy.violationReason, policy.violationAction)
+	}`, expectedCommit, expected.name, expected.gitRef, expected.slug, expected.description, expected.scopeRego, expected.conditionsRego, expected.violationReason, expected.violationAction)
 
 	jsonassert.New(t).Assertf(expectedJson, string(jsonCommand))
 }
