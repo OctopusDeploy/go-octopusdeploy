f99ab0f5f85bcf8dff8dd7c261e3ec5537fd320c
Rose Northey <rose.northey@octopus.com>
2026-08-20 09:45:28 +1200

    add marshall test for method

--- pkg/releases/release_service_test.go
+++ pkg/releases/release_service_test.go
@@ -2,22 +2,24 @@ package releases
 
 import (
 	"encoding/json"
+	"fmt"
 	"testing"
 
+	"github.com/OctopusDeploy/go-octopusdeploy/v2/internal"
 	"github.com/stretchr/testify/require"
 )
 
 func TestSnapshotVariablesByNameMarshall(t *testing.T) {
 	command := snapshotVariablesByNameCommand{Variables: []VariableIdentifier{
 		{
-			Name:    "MyVariable",
-			OwnerID: "Projects-1",
+			Name:    internal.GetRandomName(),
+			OwnerID: internal.GetRandomName(),
 		},
 	}}
 	data, err := json.Marshal(command)
 	require.NoError(t, err)
-	require.JSONEq(t, `{
-		"Variables": [{"Name":"MyVariable","OwnerId":"Projects-1"}]
-	}`, string(data))
+	jsonString := fmt.Sprintf(`{"Variables":[{"Name":"%s","OwnerId":"%s"}]}`, command.Variables[0].Name, command.Variables[0].OwnerID)
+
+	require.JSONEq(t, jsonString, string(data))
 
 }
